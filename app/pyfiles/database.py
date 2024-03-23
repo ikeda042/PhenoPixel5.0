0c7f5af46891cb257f21a3a0ae20f15abdec3c11
ikeda042 <ikeda042main@gmail.com>
2024-03-23 09:24:25 +0900

    feat: SQLite schemas

---
+++ app/pyfiles/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine, Column, Integer, String, BLOB, FLOAT
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Cell(Base):
+    __tablename__ = 'cells'
+    id = Column(Integer, primary_key=True)
+    cell_id = Column(String)
+    label_experiment = Column(String)
+    manual_label  = Column(Integer)
+    perimeter = Column(FLOAT)
+    area = Column(FLOAT)
+    img_ph = Column(BLOB) 
+    img_fluo1 = Column(BLOB)
+    img_fluo2 = Column(BLOB)
+    contour = Column(BLOB)
+    center_x = Column(FLOAT)
+    center_y = Column(FLOAT)
