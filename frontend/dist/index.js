8bbfb5eb13668c774b9f777efaf3897d220465a4
ikeda042 <ikeda042main@gmail.com>
2024-06-21 17:11:57 +0900

    software

--- frontend/dist/index.js
+++ frontend/dist/index.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const react_1 = __importDefault(require("react"));
 const react_dom_1 = __importDefault(require("react-dom"));
-const Component1_1 = __importDefault(require("./Component1"));
+const Component1_1 = __importDefault(require("./components/Component1"));
 const App = () => {
     return (react_1.default.createElement("div", null,
         react_1.default.createElement(react_1.default.Fragment, null,
