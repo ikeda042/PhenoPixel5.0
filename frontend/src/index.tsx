0bc52cdd94c6947aef30340767c7dffed7f29984
ikeda042 <ikeda042main@gmail.com>
2024-06-21 17:05:46 +0900

    CP3

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import DrawerAppBar from './Component1';
 
 const App: React.FC = () => {
     return (
         <div>
             <h1>Hello, Electron with TypeScript and React!</h1>
             {/* ここに main.ts の内容を移行します */}
+            <DrawerAppBar />
         </div>
     );
 };
