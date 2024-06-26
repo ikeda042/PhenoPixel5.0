8dccfc04507ac804d730b6dd26de5a27488b20be
ikeda042 <ikeda042main@gmail.com>
2024-06-26 12:31:07 +0900

    compile

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+    return (
+        <>
+            <h1>Hello, React with Electron!</h1>
+        </>
+    );
+}
+
+export default App;
