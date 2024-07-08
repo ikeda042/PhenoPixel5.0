f8dae6dad939c677de81e057e7521d140879a2e3
ikeda042 <ikeda042main@gmail.com>
2024-07-08 15:14:15 +0900

    Feat: Basic schemas

---
+++ backend/schemas.py
@@ -0,0 +1,71 @@
+from pydantic import BaseModel
+
+
+class CellDB(BaseModel):
+    cell_id: str
+    label_experiment: str
+    manual_label: int
+    perimeter: float
+    area: float
+
+
+class DBInfo(BaseModel):
+    file_name: str
+    cell_count: int
+
+
+class CellDBAll(BaseModel):
+    cell_id: str
+    label_experiment: str
+    manual_label: int
+    perimeter: float
+    area: float
+    img_ph: bytes
+    img_fluo1: bytes
+    img_fluo2: bytes | None
+    contour: bytes
+    center_x: float
+    center_y: float
+
+
+class BasicCellInfo(BaseModel):
+    cell_id: str
+    label_experiment: str
+    manual_label: int
+    perimeter: float
+    area: float
+
+
+class CellStats(BaseModel):
+    basic_cell_info: BasicCellInfo
+    ph_max_brightness: float | None = None
+    ph_min_brightness: float | None = None
+    ph_mean_brightness_raw: float | None = None
+    ph_mean_brightness_normalized: float | None = None
+    ph_median_brightness_raw: float | None = None
+    ph_median_brightness_normalized: float | None = None
+    max_brightness: float
+    min_brightness: float
+    mean_brightness_raw: float
+    mean_brightness_normalized: float
+    median_brightness_raw: float
+    median_brightness_normalized: float
+
+
+class CellStatsv2(BaseModel):
+    cell_id: str
+    center_x: float
+    center_y: float
+    basic_cell_info: BasicCellInfo
+    ph_max_brightness: float | None = None
+    ph_min_brightness: float | None = None
+    ph_mean_brightness_raw: float | None = None
+    ph_mean_brightness_normalized: float | None = None
+    ph_median_brightness_raw: float | None = None
+    ph_median_brightness_normalized: float | None = None
+    max_brightness: float
+    min_brightness: float
+    mean_brightness_raw: float
+    mean_brightness_normalized: float
+    median_brightness_raw: float
+    median_brightness_normalized: float
