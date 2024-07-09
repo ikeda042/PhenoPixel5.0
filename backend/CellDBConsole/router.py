014e2ffd6befe497a7bebbae3f11ab374f31f9a4
ikeda042 <ikeda042main@gmail.com>
2024-07-09 12:17:49 +0900

    router

--- backend/CellDBConsole/router.py
+++ backend/CellDBConsole/router.py
@@ -77,6 +77,11 @@ async def replot_cell(cell_id: str, degree: int = 4, db_name: str = "test_databa
     return await CellCrudBase(db_name=db_name).replot(cell_id=cell_id, degree=degree)
 
 
+# @router_cell.get("/{cell_id}/reolot_fig")
+# async def replot_cell():
+#     return
+
+
 # @router_cell.get("/{cell_id}/path")
 # async def get_cell_path(cell_id: str, db_name: str = "test_database.db"):
 #     return await CellCrudBase(db_name=db_name).path_analysis(cell_id=cell_id)
