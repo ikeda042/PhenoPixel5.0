d3d14f8f4aa315b484baf05702e7a6ef54dc74d5
ikeda042 <ikeda042main@gmail.com>
2024-07-10 23:19:58 +0900

    Reloadable

--- backend/app/main.py
+++ backend/app/main.py
@@ -26,3 +26,8 @@ async def healthcheck():
 
 
 app.include_router(router_cell, prefix=api_prefix)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
