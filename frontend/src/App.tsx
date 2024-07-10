7c3a228faebf18e938079745798e473e2543cc96
ikeda042 <ikeda042main@gmail.com>
2024-07-11 01:13:25 +0900

    unimport

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,29 +2,14 @@
 import './App.css';
 import Nav from './components/Nav';
 import { Box } from '@mui/system';
-// import SquareImage from './components/Squareimage';
-// import DBtable from './components/Dbtable';
-// import Dbcontents from './pages/Ddcontents';
 import Grid from '@mui/material/Unstable_Grid2';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
-// import Cell from './pages/Cell'
-// import DbcontentsOverview from './pages/Cellsoverview';
-// import { settings } from './settings';
-// import MCPR from './pages/MCPR';
-// import Slots from './components/Slots';
-import { settings } from './settings';
 import CellImageGrid from './components/CellOverview';
 
 
 function App() {
-  // const [data, setData] = useState([]);
 
-  // useEffect(() => {
-  //   fetch(`${settings.url_prefix}/cellapi/cells/databases`)
-  //     .then(response => response.json())
-  //     .then(data => setData(data));
-  // }, []);
 
   return (
     <Router>
