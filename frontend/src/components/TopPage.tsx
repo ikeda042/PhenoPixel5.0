7a0bb01733434a2baa96d3b29df1b03e9b1a73dc
ikeda042 <ikeda042main@gmail.com>
2024-07-12 02:52:10 +0900

    TP{

--- frontend/src/components/TopPage.tsx
+++ frontend/src/components/TopPage.tsx
@@ -1,212 +1,54 @@
-import React, { useEffect, useState } from "react";
-import { Box, Typography, Container, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, IconButton, TextField, Button, Grid, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Select, MenuItem, SelectChangeEvent } from "@mui/material";
-import axios from "axios";
-import { settings } from "../settings";
-import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import React from "react";
+import { Box, Button, Container } from "@mui/material";
 import DatabaseIcon from '@mui/icons-material/Storage';
-import FileUploadIcon from '@mui/icons-material/FileUpload';
+import ScienceIcon from '@mui/icons-material/Science';
 import { useNavigate } from "react-router-dom";
 
-interface ListDBResponse {
-    databases: string[];
-}
-
-const url_prefix = settings.url_prefix;
-
 const TopPage: React.FC = () => {
-    const [databases, setDatabases] = useState<string[]>([]);
-    const [searchQuery, setSearchQuery] = useState("");
-    const [displayMode, setDisplayMode] = useState('User uploaded');
-    const [selectedFile, setSelectedFile] = useState<File | null>(null);
-    const [dialogOpen, setDialogOpen] = useState(false);
-    const [dialogMessage, setDialogMessage] = useState("");
     const navigate = useNavigate();
 
-    useEffect(() => {
-        const fetchDatabases = async () => {
-            try {
-                const response = await axios.get<ListDBResponse>(`${url_prefix}/databases`);
-                setDatabases(response.data.databases);
-            } catch (error) {
-                console.error("Failed to fetch databases", error);
-            }
-        };
-
-        fetchDatabases();
-    }, []);
-
-    const handleNavigate = (dbName: string) => {
-        navigate(`/databases/?db_name=${dbName}`);
+    const handleNavigate = (path: string) => {
+        navigate(path);
     };
-
-    const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setSearchQuery(event.target.value);
-    };
-
-    const handleDisplayModeChange = (event: SelectChangeEvent<string>) => {
-        setDisplayMode(event.target.value);
-    };
-
-    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        if (event.target.files) {
-            setSelectedFile(event.target.files[0]);
-        }
-    };
-
-    const handleUpload = async () => {
-        if (selectedFile) {
-            const formData = new FormData();
-            formData.append("file", selectedFile);
-            try {
-                const response = await axios.post(`${url_prefix}/databases/upload`, formData, {
-                    headers: {
-                        'Content-Type': 'multipart/form-data'
-                    }
-                });
-                setDialogMessage("Database uploaded successfully!");
-                setDialogOpen(true);
-                console.log(response.data);
-            } catch (error) {
-                setDialogMessage("Failed to upload database.");
-                setDialogOpen(true);
-                console.error("Failed to upload database", error);
-            }
-        }
-    };
-
-    const handleCloseDialog = () => {
-        setDialogOpen(false);
-        window.location.reload();
-    };
-
-    const filteredDatabases = databases.filter(database => {
-        const searchMatch = database.toLowerCase().includes(searchQuery.toLowerCase());
-        if (displayMode === 'User uploaded') {
-            return searchMatch && database.endsWith('-uploaded.db');
-        }
-        if (displayMode === 'Validated') {
-            return searchMatch && !database.endsWith('-uploaded.db');
-        }
-        return searchMatch;
-    });
 
     return (
         <Container>
-            <Box display="flex" flexDirection="column" alignItems="center" justifyContent="space-between">
-                <Grid container spacing={2} alignItems="center">
-                    <Grid item xs={4}>
-                        <TextField
-                            label="Search Database"
-                            variant="outlined"
-                            fullWidth
-                            value={searchQuery}
-                            onChange={handleSearchChange}
-                            sx={{ height: '56px' }}
-                        />
-                    </Grid>
-                    <Grid item xs={2}>
-                        <Select
-                            value={displayMode}
-                            onChange={handleDisplayModeChange}
-                            displayEmpty
-                            inputProps={{ 'aria-label': 'Without label' }}
-                            fullWidth
-                            sx={{ height: '56px' }}
-                        >
-                            <MenuItem value="Validated">Validated</MenuItem>
-                            <MenuItem value="User uploaded">Uploaded</MenuItem>
-                        </Select>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <input
-                            accept=".db"
-                            style={{ display: 'none' }}
-                            id="raised-button-file"
-                            multiple
-                            type="file"
-                            onChange={handleFileChange}
-                        />
-                        <label htmlFor="raised-button-file">
-                            <Button
-                                variant="contained"
-                                component="span"
-                                startIcon={<DatabaseIcon />}
-                                sx={{
-                                    backgroundColor: 'white',
-                                    color: 'black',
-                                    width: '100%',
-                                    height: '56px',
-                                    '&:hover': {
-                                        backgroundColor: 'lightgrey'
-                                    }
-                                }}
-                            >
-                                {selectedFile ? selectedFile.name : "Select Database"}
-                            </Button>
-                        </label>
-                    </Grid>
-                    <Grid item xs={3}>
-                        <Button
-                            onClick={handleUpload}
-                            variant="contained"
-                            sx={{
-                                backgroundColor: 'black',
-                                color: 'white',
-                                width: '100%',
-                                height: '56px',
-                                '&:hover': {
-                                    backgroundColor: 'grey'
-                                }
-                            }}
-                            startIcon={<FileUploadIcon />}
-                        >
-                            Upload
-                        </Button>
-                    </Grid>
-                </Grid>
+            <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" gap={2} height="100vh">
+                <Button
+                    variant="contained"
+                    component="span"
+                    startIcon={<DatabaseIcon />}
+                    onClick={() => handleNavigate('/dbconsole')}
+                    sx={{
+                        backgroundColor: 'white',
+                        color: 'black',
+                        width: '100%',
+                        height: '56px',
+                        '&:hover': {
+                            backgroundColor: 'lightgrey'
+                        }
+                    }}
+                >
+                    Data analyses
+                </Button>
+                <Button
+                    variant="contained"
+                    component="span"
+                    startIcon={<ScienceIcon />}
+                    onClick={() => handleNavigate('/phenopixel')}
+                    sx={{
+                        backgroundColor: 'white',
+                        color: 'black',
+                        width: '100%',
+                        height: '56px',
+                        '&:hover': {
+                            backgroundColor: 'lightgrey'
+                        }
+                    }}
+                >
+                    Cell extraction
+                </Button>
             </Box>
-
-            <Box mt={3}>
-                <TableContainer component={Paper}>
-                    <Table>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>Database Name</TableCell>
-                                <TableCell align="right">Go</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {filteredDatabases.map((database, index) => (
-                                <TableRow key={index}>
-                                    <TableCell component="th" scope="row">
-                                        {database}
-                                    </TableCell>
-                                    <TableCell align="right">
-                                        <IconButton onClick={() => handleNavigate(database)}>
-                                            <Typography>Access database </Typography>
-                                            <NavigateNextIcon />
-                                        </IconButton>
-                                    </TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-            </Box>
-
-            <Dialog open={dialogOpen} onClose={handleCloseDialog}>
-                <DialogTitle>{"File Upload Status"}</DialogTitle>
-                <DialogContent>
-                    <DialogContentText>
-                        {dialogMessage}
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleCloseDialog} color="primary">
-                        Close
-                    </Button>
-                </DialogActions>
-            </Dialog>
         </Container>
     );
 };
