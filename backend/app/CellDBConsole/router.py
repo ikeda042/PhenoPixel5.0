2802e9fe6e6afa467102748981db19241ce9b057
ikeda042 <ikeda042main@gmail.com>
2024-07-12 01:40:21 +0900

    router

--- backend/app/CellDBConsole/router.py
+++ backend/app/CellDBConsole/router.py
@@ -152,9 +152,7 @@ async def get_heatmap(db_name: str, label: str, cell_id: str):
 )
 async def get_heatmap_csv(db_name: str, label: str, cell_id: str):
     await AsyncChores().validate_database_name(db_name)
-    return await CellCrudBase(db_name=db_name).get_peak_path_csv(
-        label=label, cell_id=cell_id
-    )
+    return await CellCrudBase(db_name=db_name).get_peak_path_csv(cell_id=cell_id)
 
 
 @router_database.post("/upload")
