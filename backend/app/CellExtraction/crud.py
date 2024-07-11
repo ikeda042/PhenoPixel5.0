b9a12c4d72b0011b4ad03e3cc105f23a23852db3
ikeda042 <ikeda042main@gmail.com>
2024-07-12 03:13:28 +0900

    session

---
+++ backend/app/CellExtraction/crud.py
@@ -0,0 +1,324 @@
+import nd2reader
+import numpy as np
+from PIL import Image
+import os
+
+
+class SyncChores:
+    @staticmethod
+    def process_image(array):
+        """
+        画像処理関数：正規化とスケーリングを行う。
+        """
+        array = array.astype(np.float32)  # Convert to float
+        array -= array.min()  # Normalize to 0
+        array /= array.max()  # Normalize to 1
+        array *= 255  # Scale to 0-255
+        return array.astype(np.uint8)
+
+    @staticmethod
+    def extract_nd2(file_name: str):
+        """
+        nd2ファイルをMultipageTIFFに変換する。
+        """
+        try:
+            os.mkdir("nd2totiff")
+        except FileExistsError:
+            pass
+
+        with nd2reader.ND2Reader(file_name) as images:
+            # 利用可能な軸とサイズをチェック
+            print(f"Available axes: {images.axes}")
+            print(f"Sizes: {images.sizes}")
+
+            # チャンネル情報があるかどうかに基づいて軸を設定
+            images.bundle_axes = "cyx" if "c" in images.axes else "yx"
+            images.iter_axes = "v"
+
+            # チャンネル数を取得（なければデフォルトで1）
+            num_channels = images.sizes.get("c", 1)
+            print(f"Total images: {len(images)}")
+            print(f"Channels: {num_channels}")
+            print("##############################################")
+
+            # チャンネル名を取得（なければデフォルト名を使用）
+            channels = images.metadata.get("channels", ["Default"])
+
+            for n, img in enumerate(images):
+                if num_channels > 1:
+                    for channel in range(num_channels):
+                        array = np.array(img[channel])
+                        array = SyncChores.process_image(array)
+                        image = Image.fromarray(array)
+                        image.save(f"nd2totiff/image_{n}_channel_{channel}.tif")
+                else:
+                    array = np.array(img)
+                    array = SyncChores.process_image(array)
+                    image = Image.fromarray(array)
+                    image.save(f"nd2totiff/image_{n}.tif")
+
+            all_images = []
+            for i in range(len(images)):
+                if num_channels > 1:
+                    for j in range(num_channels):
+                        all_images.append(
+                            Image.open(f"nd2totiff/image_{i}_channel_{j}.tif")
+                        )
+                else:
+                    all_images.append(Image.open(f"nd2totiff/image_{i}.tif"))
+
+            all_images[0].save(
+                f"{file_name.split('/')[-1].split('.')[0]}.tif",
+                save_all=True,
+                append_images=all_images[1:],
+            )
+
+
+# if __name__ == "__main__":
+#     SyncChores().extract_nd2(
+#         "/Users/leeyunosuke/Documents/PhenoPixel5.0/sk326tri30min.nd2"
+#     )
+
+
+# from .initialize import init
+# from .unify_images import unify_images_ndarray2, unify_images_ndarray
+# from .database import Cell, Base
+# from .calc_center import get_contour_center
+import os
+import cv2
+from tqdm import tqdm
+import pickle
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from typing import Literal
+from database import get_session, Cell
+
+
+# def image_process(
+#     input_filename: str,
+#     param1: int = 80,
+#     image_size: int = 100,
+#     mode: Literal["single_layer", "dual_layer", "triple_layer"] = "dual_layer",
+# ) -> None:
+#     engine = create_engine(f'sqlite:///{input_filename.split(".")[0]}.db', echo=False)
+#     Base.metadata.create_all(engine)
+#     Session = sessionmaker(bind=engine)
+#     num_tif = init(
+#         input_filename=input_filename,
+#         param1=param1,
+#         param2=param2,
+#         image_size=image_size,
+#         fluo_dual_layer_mode=fluo_dual_layer_mode,
+#         single_layer_mode=single_layer_mode,
+#     )
+#     print(num_tif)
+#     print(
+#         "Processing images...\n+\n+\n+\n+\n+\n+\n+\n+\n+\n+\n+\n+\n+\n+\n+\n+\n+\n+\n+\n+\n"
+#     )
+#     iter_n = num_tif // 3 if not single_layer_mode else num_tif
+#     for k in tqdm(range(0, iter_n)):
+#         for j in range(len(os.listdir(f"TempData/frames/tiff_{k}/Cells/ph/"))):
+#             cell_id: str = f"F{k}C{j}"
+#             img_ph = cv2.imread(f"TempData/frames/tiff_{k}/Cells/ph/{j}.png")
+#             if not single_layer_mode:
+#                 img_fluo1 = cv2.imread(f"TempData/frames/tiff_{k}/Cells/fluo1/{j}.png")
+
+#             img_ph_gray = cv2.cvtColor(img_ph, cv2.COLOR_BGR2GRAY)
+#             if not single_layer_mode:
+#                 img_fluo1_gray = cv2.cvtColor(img_fluo1, cv2.COLOR_BGR2GRAY)
+
+#             ret, thresh = cv2.threshold(img_ph_gray, param1, param2, cv2.THRESH_BINARY)
+#             img_canny = cv2.Canny(thresh, 0, 150)
+#             contours_raw, hierarchy = cv2.findContours(
+#                 img_canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE
+#             )
+#             # Filter out contours with small area
+#             contours = list(filter(lambda x: cv2.contourArea(x) >= 300, contours_raw))
+#             # Check if the center of the contour is not too far from the center of the image
+#             contours = list(
+#                 filter(
+#                     lambda x: abs(
+#                         cv2.moments(x)["m10"] / cv2.moments(x)["m00"] - image_size / 2
+#                     )
+#                     < 10,
+#                     contours,
+#                 )
+#             )
+#             # do the same for y
+#             contours = list(
+#                 filter(
+#                     lambda x: abs(
+#                         cv2.moments(x)["m01"] / cv2.moments(x)["m00"] - image_size / 2
+#                     )
+#                     < 10,
+#                     contours,
+#                 )
+#             )
+
+#             if not single_layer_mode:
+#                 cv2.drawContours(img_fluo1, contours, -1, (0, 255, 0), 1)
+#                 cv2.imwrite(
+#                     f"TempData/frames/tiff_{k}/Cells/fluo1_contour/{j}.png", img_fluo1
+#                 )
+#             cv2.drawContours(img_ph, contours, -1, (0, 255, 0), 1)
+#             cv2.imwrite(f"TempData/frames/tiff_{k}/Cells/ph_contour/{j}.png", img_ph)
+
+#             if fluo_dual_layer_mode:
+#                 img_fluo2 = cv2.imread(f"TempData/frames/tiff_{k}/Cells/fluo2/{j}.png")
+#                 print(f"empData/frames/tiff_{k}/Cells/fluo2/{j}.png")
+#                 img_fluo2_gray = cv2.cvtColor(img_fluo2, cv2.COLOR_BGR2GRAY)
+#                 cv2.drawContours(img_fluo2, contours, -1, (0, 255, 0), 1)
+#                 cv2.imwrite(
+#                     f"TempData/frames/tiff_{k}/Cells/fluo2_contour/{j}.png", img_fluo2
+#                 )
+
+#             if contours != []:
+#                 if draw_scale_bar:
+#                     image_ph_copy = img_ph.copy()
+#                     if not single_layer_mode:
+#                         image_fluo1_copy = img_fluo1.copy()
+
+#                     pixel_per_micro_meter = 0.0625
+#                     # want to draw a scale bar of 20% of the image width at the bottom right corner. (put some mergins so that the scale bar is not too close to the edge)
+#                     # scale bar length in pixels
+#                     scale_bar_length = int(image_size * 0.2)
+#                     scale_bar_size = scale_bar_length * pixel_per_micro_meter
+#                     # scale bar thickness in pixels
+#                     scale_bar_thickness = int(2 * (image_size / 100))
+#                     # scale bar mergins from the edge of the image
+#                     scale_bar_mergins = int(10 * (image_size / 100))
+#                     # scale bar color
+#                     scale_bar_color = (255, 255, 255)
+#                     # scale bar text color
+#                     scale_bar_text_color = (255, 255, 255)
+#                     # draw scale bar for the both image_ph and image_fluo and the scale bar should be Rectangle
+#                     # scale bar for image_ph
+#                     cv2.rectangle(
+#                         image_ph_copy,
+#                         (
+#                             image_size - scale_bar_mergins - scale_bar_length,
+#                             image_size - scale_bar_mergins,
+#                         ),
+#                         (
+#                             image_size - scale_bar_mergins,
+#                             image_size - scale_bar_mergins - scale_bar_thickness,
+#                         ),
+#                         scale_bar_color,
+#                         -1,
+#                     )
+#                     # cv2.putText(image_ph_copy,f"{round(scale_bar_size,2)} µm",(image_size-scale_bar_mergins-scale_bar_length,image_size-scale_bar_mergins-2*scale_bar_thickness),cv2.FONT_HERSHEY_SIMPLEX,0.2,scale_bar_text_color,1,cv2.LINE_AA)
+#                     # scale bar for image_fluo
+#                     if not single_layer_mode:
+#                         cv2.rectangle(
+#                             image_fluo1_copy,
+#                             (
+#                                 image_size - scale_bar_mergins - scale_bar_length,
+#                                 image_size - scale_bar_mergins,
+#                             ),
+#                             (
+#                                 image_size - scale_bar_mergins,
+#                                 image_size - scale_bar_mergins - scale_bar_thickness,
+#                             ),
+#                             scale_bar_color,
+#                             -1,
+#                         )
+#                     # cv2.putText(image_fluo_copy,f"{round(scale_bar_size,2)} µm",(image_size-scale_bar_mergins-scale_bar_length,image_size-scale_bar_mergins-2*scale_bar_thickness),cv2.FONT_HERSHEY_SIMPLEX,0.2,scale_bar_text_color,1,cv2.LINE_AA)
+#                     if fluo_dual_layer_mode:
+#                         image_fluo2_copy = img_fluo2.copy()
+#                         cv2.rectangle(
+#                             image_fluo2_copy,
+#                             (
+#                                 image_size - scale_bar_mergins - scale_bar_length,
+#                                 image_size - scale_bar_mergins,
+#                             ),
+#                             (
+#                                 image_size - scale_bar_mergins,
+#                                 image_size - scale_bar_mergins - scale_bar_thickness,
+#                             ),
+#                             scale_bar_color,
+#                             -1,
+#                         )
+#                         unify_images_ndarray2(
+#                             image1=image_ph_copy,
+#                             image2=image_fluo1_copy,
+#                             image3=image_fluo2_copy,
+#                             output_name=f"TempData/frames/tiff_{k}/Cells/unified_images/{j}",
+#                         )
+#                         unify_images_ndarray2(
+#                             image1=image_ph_copy,
+#                             image2=image_fluo1_copy,
+#                             image3=image_fluo2_copy,
+#                             output_name=f"TempData/app_data/{cell_id}",
+#                         )
+#                     elif single_layer_mode:
+#                         cv2.imwrite(
+#                             f"TempData/frames/tiff_{k}/Cells/unified_images/{j}.png",
+#                             image_ph_copy,
+#                         )
+#                         cv2.imwrite(f"TempData/app_data/{cell_id}.png", image_ph_copy)
+#                     else:
+#                         unify_images_ndarray(
+#                             image1=image_ph_copy,
+#                             image2=image_fluo1_copy,
+#                             output_name=f"TempData/frames/tiff_{k}/Cells/unified_images/{j}",
+#                         )
+#                         unify_images_ndarray(
+#                             image1=image_ph_copy,
+#                             image2=image_fluo1_copy,
+#                             output_name=f"TempData/app_data/{cell_id}",
+#                         )
+
+#                 with Session() as session:
+#                     perimeter = cv2.arcLength(contours[0], closed=True)
+#                     area = cv2.contourArea(contour=contours[0])
+#                     image_ph_data = cv2.imencode(".png", img_ph_gray)[1].tobytes()
+#                     if not single_layer_mode:
+#                         image_fluo1_data = cv2.imencode(".png", img_fluo1_gray)[
+#                             1
+#                         ].tobytes()
+#                     if fluo_dual_layer_mode:
+#                         image_fluo2_data = cv2.imencode(".png", img_fluo2_gray)[
+#                             1
+#                         ].tobytes()
+#                     contour = pickle.dumps(contours[0])
+#                     center_x, center_y = get_contour_center(contours[0])
+#                     print(center_x, center_y)
+#                     if fluo_dual_layer_mode:
+#                         cell = Cell(
+#                             cell_id=cell_id,
+#                             label_experiment="",
+#                             perimeter=perimeter,
+#                             area=area,
+#                             img_ph=image_ph_data,
+#                             img_fluo1=image_fluo1_data,
+#                             img_fluo2=image_fluo2_data,
+#                             contour=contour,
+#                             center_x=center_x,
+#                             center_y=center_y,
+#                         )
+#                     elif single_layer_mode:
+#                         cell = Cell(
+#                             cell_id=cell_id,
+#                             label_experiment="",
+#                             perimeter=perimeter,
+#                             area=area,
+#                             img_ph=image_ph_data,
+#                             contour=contour,
+#                             center_x=center_x,
+#                             center_y=center_y,
+#                         )
+#                     else:
+#                         cell = Cell(
+#                             cell_id=cell_id,
+#                             label_experiment="",
+#                             perimeter=perimeter,
+#                             area=area,
+#                             img_ph=image_ph_data,
+#                             img_fluo1=image_fluo1_data,
+#                             contour=contour,
+#                             center_x=center_x,
+#                             center_y=center_y,
+#                         )
+#                     if session.query(Cell).filter_by(cell_id=cell_id).first() is None:
+#                         session.add(cell)
+#                         session.commit()
