5451cb0070488c54e54dc520bc24a268ef3b18d7
ikeda042 <ikeda042main@gmail.com>
2024-07-11 23:14:34 +0900

    ylabe;

--- backend/app/CellDBConsole/router.py
+++ backend/app/CellDBConsole/router.py
@@ -100,7 +100,9 @@ async def get_mean_fluo_intensities(db_name: str, label: str, cell_id: str):
     await AsyncChores().validate_database_name(db_name)
     ret: list[float] = await CellCrudBase(
         db_name=db_name
-    ).get_all_mean_normalized_fluo_intensities(label=label, cell_id=cell_id)
+    ).get_all_mean_normalized_fluo_intensities(
+        label=label, cell_id=cell_id, y_label="Normalized Fluorescence Intensity"
+    )
     return ret
 
 
@@ -109,7 +111,9 @@ async def get_median_fluo_intensities(db_name: str, label: str, cell_id: str):
     await AsyncChores().validate_database_name(db_name)
     ret: list[float] = await CellCrudBase(
         db_name=db_name
-    ).get_all_median_normalized_fluo_intensities(label=label, cell_id=cell_id)
+    ).get_all_median_normalized_fluo_intensities(
+        label=label, cell_id=cell_id, y_label="Normalized Fluorescence Intensity"
+    )
     return ret
 
 
