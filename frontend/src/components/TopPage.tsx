e73a6102b892637db83b7ea79497979285b67230
ikeda042 <ikeda042main@gmail.com>
2024-07-11 21:23:20 +0900

    force reload

--- frontend/src/components/TopPage.tsx
+++ frontend/src/components/TopPage.tsx
@@ -76,6 +76,7 @@ const TopPage: React.FC = () => {
 
     const handleCloseDialog = () => {
         setDialogOpen(false);
+        window.location.reload();
     };
 
     const filteredDatabases = databases.filter(database => {
