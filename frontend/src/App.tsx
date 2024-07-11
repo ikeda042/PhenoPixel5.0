330a9f884ce0c92cd64b55276f3486d57a23d88a
ikeda042 <ikeda042main@gmail.com>
2024-07-12 02:43:07 +0900

    Rougin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,12 +7,21 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
 import CellImageGrid from './components/CellOverview';
 import Databases from './components/Databases';
+import TopPage from './components/TopPage';
 
 function App() {
   return (
     <Router>
       <Box sx={{ bgcolor: "#f7f6f5", color: 'black', minHeight: '100vh' }}>
         <Routes>
+          <Route path="/" element={
+            <>
+              <Nav title='PhenoPixel5.0' />
+              <Grid container spacing={4} margin={5} mt={-4}>
+                <TopPage />
+              </Grid >
+            </>
+          } />
           <Route path="/databases" element={
             <>
               <Nav title='PhenoPixel5.0' />
