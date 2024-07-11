3bc803160f5461f20990fe6aa9f6d7046292b0c8
ikeda042 <ikeda042main@gmail.com>
2024-07-12 00:11:09 +0900

    router

--- backend/app/CellDBConsole/router.py
+++ backend/app/CellDBConsole/router.py
@@ -117,6 +117,22 @@ async def get_median_fluo_intensities(db_name: str, label: str, cell_id: str):
     return ret
 
 
+@router_cell.get("/{db_name}/{label}/{cell_id}/mean_ph_intensities/csv")
+async def get_mean_ph_intensities_csv(db_name: str, label: str, cell_id: str):
+    await AsyncChores().validate_database_name(db_name)
+    return await CellCrudBase(
+        db_name=db_name
+    ).get_all_mean_normalized_fluo_intensities_csv(label=label)
+
+
+@router_cell.get("/{db_name}/{label}/{cell_id}/mean_fluo_intensities/csv")
+async def get_mean_fluo_intensities_csv(db_name: str, label: str, cell_id: str):
+    await AsyncChores().validate_database_name(db_name)
+    return await CellCrudBase(
+        db_name=db_name
+    ).get_all_mean_normalized_fluo_intensities_csv(label=label)
+
+
 @router_database.post("/upload")
 async def upload_database(file: UploadFile = UploadFile(...)):
     db_name = file.filename
