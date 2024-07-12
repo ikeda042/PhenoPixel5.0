a1a85c5629961e81af36b564a41e5106c38014bb
ikeda042 <ikeda042main@gmail.com>
2024-07-12 16:11:34 +0900

    CRUD

--- backend/app/CellExtraction/crud.py
+++ backend/app/CellExtraction/crud.py
@@ -11,6 +11,44 @@ import numpy as np
 import asyncio
 import pickle
 from sqlalchemy.sql import select
+from sqlalchemy import Column, Integer, String, BLOB, FLOAT
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+import os
+
+
+Base = declarative_base()
+
+
+class Cell(Base):
+    __tablename__ = "cells"
+    id = Column(Integer, primary_key=True)
+    cell_id = Column(String)
+    label_experiment = Column(String)
+    manual_label = Column(Integer)
+    perimeter = Column(FLOAT)
+    area = Column(FLOAT)
+    img_ph = Column(BLOB)
+    img_fluo1 = Column(BLOB, nullable=True)
+    img_fluo2 = Column(BLOB, nullable=True)
+    contour = Column(BLOB)
+    center_x = Column(FLOAT)
+    center_y = Column(FLOAT)
+
+
+async def get_session(dbname: str):
+    engine = create_async_engine(f"sqlite+aiosqlite:///{dbname}", echo=False)
+    async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+    async with async_session() as session:
+        yield session
+
+
+async def create_database(dbname: str):
+    engine = create_async_engine(f"sqlite+aiosqlite:///{dbname}", echo=True)
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    return engine
 
 
 class SyncChores:
@@ -312,46 +350,6 @@ class SyncChores:
                         )
                         n += 1
         return num_tiff
-
-
-from sqlalchemy import Column, Integer, String, BLOB, FLOAT
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-import os
-
-
-Base = declarative_base()
-
-
-class Cell(Base):
-    __tablename__ = "cells"
-    id = Column(Integer, primary_key=True)
-    cell_id = Column(String)
-    label_experiment = Column(String)
-    manual_label = Column(Integer)
-    perimeter = Column(FLOAT)
-    area = Column(FLOAT)
-    img_ph = Column(BLOB)
-    img_fluo1 = Column(BLOB, nullable=True)
-    img_fluo2 = Column(BLOB, nullable=True)
-    contour = Column(BLOB)
-    center_x = Column(FLOAT)
-    center_y = Column(FLOAT)
-
-
-async def get_session(dbname: str):
-    engine = create_async_engine(f"sqlite+aiosqlite:///{dbname}", echo=False)
-    async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
-    async with async_session() as session:
-        yield session
-
-
-async def create_database(dbname: str):
-    engine = create_async_engine(f"sqlite+aiosqlite:///{dbname}", echo=True)
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-    return engine
 
 
 class CellExtraction:
