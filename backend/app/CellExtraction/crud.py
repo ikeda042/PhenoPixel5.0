84e1e94cdb45bbb34ca34a60f2f3e944f369fa34
ikeda042 <ikeda042main@gmail.com>
2024-07-13 03:59:35 +0900

    typo

--- backend/app/CellExtraction/crud.py
+++ backend/app/CellExtraction/crud.py
@@ -484,5 +484,5 @@ class ExtractionCrudBase:
         # await asyncio.to_thread(os.remove, f"uploaded_files/{self.file_prefix}.nd2")
         return dbname.split("/")[-1]
 
-    async def get_ne2_filenames(self) -> list[str]:
+    async def get_nd2_filenames(self) -> list[str]:
         return os.listdir("uploaded_files")
