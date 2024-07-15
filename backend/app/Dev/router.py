ed9ec22e4f1085a5e9780825ba53dd8c4a3c6f27
ikeda042 <ikeda042main@gmail.com>
2024-07-15 20:15:13 +0900

    dev

---
+++ backend/app/Dev/router.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter
+from fastapi.responses import JSONResponse
+import os
+from fastapi import UploadFile
+import aiofiles
+from fastapi import HTTPException
+
+router_dev = APIRouter(prefix="/dev", tags=["dev"])
+
+
+@router_dev.post("/xlsx_files")
+async def upload_xlsx_file(file: UploadFile):
+    file_path = file.filename
+    file_path = os.path.join("uploaded_files", file_path)
+    try:
+        async with aiofiles.open(file_path, "wb") as out_file:
+            while content := await file.read(1024 * 1024 * 100):
+                await out_file.write(content)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    return JSONResponse(content={"filename": file.filename})
