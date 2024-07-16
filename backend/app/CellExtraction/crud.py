492cdd5034b7a3b0f1bac42c4236b42acbbe774f
ikeda042 <ikeda042main@gmail.com>
2024-07-16 17:30:24 +0900

    CRUD

--- backend/app/CellExtraction/crud.py
+++ backend/app/CellExtraction/crud.py
@@ -3,7 +3,11 @@ import numpy as np
 from PIL import Image
 from typing import Literal
 import nd2reader
+import os
 import cv2
+import numpy as np
+import cv2
+import numpy as np
 import asyncio
 import pickle
 from sqlalchemy.sql import select
@@ -11,6 +15,7 @@ from sqlalchemy import Column, Integer, String, BLOB, FLOAT
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+import os
 import aiofiles
 from fastapi.responses import StreamingResponse
 from fastapi import HTTPException
@@ -51,30 +56,32 @@ async def create_database(dbname: str):
 class SyncChores:
     @staticmethod
     def process_image(array):
-        array = array.astype(np.float32)
-        array -= array.min()
-        array /= array.max()
-        array *= 255
+        """
+        画像処理関数：正規化とスケーリングを行う。
+        """
+        array = array.astype(np.float32)  # Convert to float
+        array -= array.min()  # Normalize to 0
+        array /= array.max()  # Normalize to 1
+        array *= 255  # Scale to 0-255
         return array.astype(np.uint8)
 
     @staticmethod
     def save_images(images, file_name, num_channels):
+        """
+        画像を保存し、MultipageTIFFとして出力する。
+        """
         all_images = []
         for i, img in enumerate(images):
             if num_channels > 1:
                 for j in range(num_channels):
                     all_images.append(
-                        Image.open(
-                            os.path.join("nd2totiff", f"image_{i}_channel_{j}.tif")
-                        )
+                        Image.open(f"nd2totiff/image_{i}_channel_{j}.tif")
                     )
             else:
-                all_images.append(
-                    Image.open(os.path.join("nd2totiff", f"image_{i}.tif"))
-                )
+                all_images.append(Image.open(f"nd2totiff/image_{i}.tif"))
 
         all_images[0].save(
-            os.path.join(f"{file_name.split(os.sep)[-1].split('.')[0]}.tif"),
+            f"{file_name.split('/')[-1].split('.')[0]}.tif",
             save_all=True,
             append_images=all_images[1:],
         )
@@ -88,6 +95,9 @@ class SyncChores:
 
     @staticmethod
     def extract_nd2(file_name: str, mode: str):
+        """
+        nd2ファイルをMultipageTIFFに変換する。
+        """
         os.makedirs("nd2totiff", exist_ok=True)
 
         with nd2reader.ND2Reader(file_name) as images:
@@ -108,26 +118,19 @@ class SyncChores:
                         array = np.array(img[channel])
                         array = SyncChores.process_image(array)
                         image = Image.fromarray(array)
-                        image.save(
-                            os.path.join(
-                                "nd2totiff", f"image_{n}_channel_{channel}.tif"
-                            )
-                        )
+                        image.save(f"nd2totiff/image_{n}_channel_{channel}.tif")
                 else:
                     array = np.array(img)
                     array = SyncChores.process_image(array)
                     image = Image.fromarray(array)
-                    image.save(os.path.join("nd2totiff", f"image_{n}.tif"))
+                    image.save(f"nd2totiff/image_{n}.tif")
 
             SyncChores.save_images(images, file_name, num_channels)
         SyncChores.cleanup("nd2totiff")
         num_tiff = SyncChores.extract_tiff(
-            tiff_path=os.path.join(
-                f"./{file_name.split(os.sep)[-1].split('.')[0]}.tif"
-            ),
-            mode=mode,
+            tiff_path=f"./{file_name.split('/')[-1].split('.')[0]}.tif", mode=mode
         )
-        os.remove(os.path.join(f"./{file_name.split(os.sep)[-1].split('.')[0]}.tif"))
+        os.remove(f"./{file_name.split('/')[-1].split('.')[0]}.tif")
         return num_tiff
 
     @staticmethod
@@ -149,7 +152,7 @@ class SyncChores:
         }
 
         for layer in layers.get(mode, []):
-            os.makedirs(os.path.join("TempData", layer), exist_ok=True)
+            os.makedirs(f"TempData/{layer}", exist_ok=True)
 
         with Image.open(tiff_path) as tiff:
             num_pages = tiff.n_frames
@@ -165,7 +168,7 @@ class SyncChores:
                 tiff.seek(i)
                 layer_idx = i % len(layer_map[mode])
                 layer = layer_map[mode][layer_idx][1]
-                filename = os.path.join("TempData", layer, f"{img_num}.tif")
+                filename = f"TempData/{layer}/{img_num}.tif"
                 print(filename)
                 tiff.save(filename, format="TIFF")
                 if layer_idx == len(layer_map[mode]) - 1:
@@ -175,6 +178,9 @@ class SyncChores:
 
     @staticmethod
     def cleanup(directory: str):
+        """
+        指定されたディレクトリを削除する。
+        """
         for root, dirs, files in os.walk(directory, topdown=False):
             for name in files:
                 os.remove(os.path.join(root, name))
@@ -184,6 +190,7 @@ class SyncChores:
 
     @staticmethod
     def get_contour_center(contour):
+        # 輪郭のモーメントを計算して重心を求める
         M = cv2.moments(contour)
         cx = int(M["m10"] / M["m00"])
         cy = int(M["m01"] / M["m00"])
@@ -193,12 +200,16 @@ class SyncChores:
     def crop_contours(image, contours, output_size):
         cropped_images = []
         for contour in contours:
+            # 各輪郭の中心座標を取得
             cx, cy = SyncChores.get_contour_center(contour)
+            # 　中心座標が画像の中心から離れているものを除外
             if cx > 400 and cx < 2000 and cy > 400 and cy < 2000:
+                # 切り抜く範囲を計算
                 x1 = max(0, cx - output_size[0] // 2)
                 y1 = max(0, cy - output_size[1] // 2)
                 x2 = min(image.shape[1], cx + output_size[0] // 2)
                 y2 = min(image.shape[0], cy + output_size[1] // 2)
+                # 画像を切り抜く
                 cropped = image[y1:y2, x1:x2]
                 cropped_images.append(cropped)
         return cropped_images
@@ -211,6 +222,7 @@ class SyncChores:
         image_size: int = 200,
         mode: Literal["single_layer", "dual_layer", "triple_layer"] = "dual_layer",
     ) -> int:
+
         if mode == "triple_layer":
             set_num = 3
             init_folders = ["Fluo1", "Fluo2", "PH", "frames", "app_data"]
@@ -226,7 +238,7 @@ class SyncChores:
         except:
             pass
 
-        init_folders = [os.path.join("TempData", d) for d in init_folders]
+        init_folders = [f"TempData/{d}" for d in init_folders]
         folders = [
             folder
             for folder in os.listdir("TempData")
@@ -237,66 +249,61 @@ class SyncChores:
                 os.mkdir(f"{i}")
             except:
                 continue
-
+        # フォルダの作成
         for i in range(num_tiff // set_num):
             try:
-                os.mkdir(os.path.join("TempData", "frames", f"tiff_{i}"))
+                os.mkdir(f"TempData/frames/tiff_{i}")
             except Exception as e:
                 print(e)
             try:
-                os.mkdir(os.path.join("TempData", "frames", f"tiff_{i}", "Cells"))
+                os.mkdir(f"TempData/frames/tiff_{i}/Cells")
             except Exception as e:
                 print(e)
             try:
-                os.mkdir(os.path.join("TempData", "frames", f"tiff_{i}", "Cells", "ph"))
+                os.mkdir(f"TempData/frames/tiff_{i}/Cells/ph")
             except Exception as e:
                 print(e)
             try:
-                os.mkdir("ph_contours")
+                os.mkdir(f"TempData/frames/tiff_{i}/Cells/fluo1")
+            except Exception as e:
+                print(e)
+            try:
+                os.mkdir(f"ph_contours")
             except Exception as e:
                 print(e)
 
             if mode == "triple_layer":
                 try:
-                    os.mkdir(
-                        os.path.join(
-                            "TempData", "frames", f"tiff_{i}", "Cells", "fluo2"
-                        )
-                    )
+                    os.mkdir(f"TempData/frames/tiff_{i}/Cells/fluo2")
                 except Exception as e:
                     print(e)
                 try:
-                    os.mkdir(
-                        os.path.join(
-                            "TempData", "frames", f"tiff_{i}", "Cells", "fluo2_adjusted"
-                        )
-                    )
+                    os.mkdir(f"TempData/frames/tiff_{i}/Cells/fluo2_adjusted")
                 except Exception as e:
                     print(e)
                 try:
-                    os.mkdir(
-                        os.path.join(
-                            "TempData", "frames", f"tiff_{i}", "Cells", "fluo2_contour"
-                        )
-                    )
+                    os.mkdir(f"TempData/frames/tiff_{i}/Cells/fluo2_contour")
                 except Exception as e:
                     print(e)
         loop_num = num_tiff // set_num if mode != "single_layer" else num_tiff
         for k in range(loop_num):
-            image_ph = cv2.imread(os.path.join("TempData", "PH", f"{k}.tif"))
+            image_ph = cv2.imread(f"TempData/PH/{k}.tif")
             print(num_tiff)
             if mode == "dual_layer" or mode == "triple_layer":
-                image_fluo_1 = cv2.imread(os.path.join("TempData", "Fluo1", f"{k}.tif"))
+                image_fluo_1 = cv2.imread(f"TempData/Fluo1/{k}.tif")
             if mode == "triple_layer":
-                image_fluo_2 = cv2.imread(os.path.join("TempData", "Fluo2", f"{k}.tif"))
+                image_fluo_2 = cv2.imread(f"TempData/Fluo2/{k}.tif")
             img_gray = cv2.cvtColor(image_ph, cv2.COLOR_BGR2GRAY)
 
+            # ２値化を行う
             ret, thresh = cv2.threshold(img_gray, param1, 255, cv2.THRESH_BINARY)
             img_canny = cv2.Canny(thresh, 0, 130)
             contours, hierarchy = cv2.findContours(
                 img_canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE
             )
+            # 細胞の面積で絞り込み
             contours = list(filter(lambda x: cv2.contourArea(x) >= 300, contours))
+            # 中心座標が画像の中心から離れているものを除外
             contours = list(
                 filter(
                     lambda x: cv2.moments(x)["m10"] / cv2.moments(x)["m00"] > 400
@@ -304,6 +311,7 @@ class SyncChores:
                     contours,
                 )
             )
+            # do the same for y
             contours = list(
                 filter(
                     lambda x: cv2.moments(x)["m01"] / cv2.moments(x)["m00"] > 400
@@ -328,7 +336,7 @@ class SyncChores:
 
             image_ph_copy = image_ph.copy()
             cv2.drawContours(image_ph_copy, contours, -1, (0, 255, 0), 3)
-            cv2.imwrite(os.path.join("ph_contours", f"{k}.png"), image_ph_copy)
+            cv2.imwrite(f"ph_contours/{k}.png", image_ph_copy)
             n = 0
             if mode == "triple_layer":
                 for ph, fluo1, fluo2 in zip(
@@ -342,79 +350,26 @@ class SyncChores:
                         and len(fluo2) == output_size[0]
                         and len(fluo2[0]) == output_size[1]
                     ):
+                        cv2.imwrite(f"TempData/frames/tiff_{k}/Cells/ph/{n}.png", ph)
                         cv2.imwrite(
-                            os.path.join(
-                                "TempData",
-                                "frames",
-                                f"tiff_{k}",
-                                "Cells",
-                                "ph",
-                                f"{n}.png",
-                            ),
-                            ph,
+                            f"TempData/frames/tiff_{k}/Cells/fluo1/{n}.png", fluo1
                         )
                         cv2.imwrite(
-                            os.path.join(
-                                "TempData",
-                                "frames",
-                                f"tiff_{k}",
-                                "Cells",
-                                "fluo1",
-                                f"{n}.png",
-                            ),
-                            fluo1,
-                        )
-                        cv2.imwrite(
-                            os.path.join(
-                                "TempData",
-                                "frames",
-                                f"tiff_{k}",
-                                "Cells",
-                                "fluo2",
-                                f"{n}.png",
-                            ),
-                            fluo2,
+                            f"TempData/frames/tiff_{k}/Cells/fluo2/{n}.png", fluo2
                         )
                         n += 1
+
             elif mode == "single_layer":
                 for ph in zip(cropped_images_ph):
                     if len(ph) == output_size[0] and len(ph[0]) == output_size[1]:
-                        cv2.imwrite(
-                            os.path.join(
-                                "TempData",
-                                "frames",
-                                f"tiff_{k}",
-                                "Cells",
-                                "ph",
-                                f"{n}.png",
-                            ),
-                            ph,
-                        )
+                        cv2.imwrite(f"TempData/frames/tiff_{k}/Cells/ph/{n}.png", ph)
                         n += 1
             elif mode == "dual_layer":
                 for ph, fluo1 in zip(cropped_images_ph, cropped_images_fluo_1):
                     if len(ph) == output_size[0] and len(ph[0]) == output_size[1]:
+                        cv2.imwrite(f"TempData/frames/tiff_{k}/Cells/ph/{n}.png", ph)
                         cv2.imwrite(
-                            os.path.join(
-                                "TempData",
-                                "frames",
-                                f"tiff_{k}",
-                                "Cells",
-                                "ph",
-                                f"{n}.png",
-                            ),
-                            ph,
-                        )
-                        cv2.imwrite(
-                            os.path.join(
-                                "TempData",
-                                "frames",
-                                f"tiff_{k}",
-                                "Cells",
-                                "fluo1",
-                                f"{n}.png",
-                            ),
-                            fluo1,
+                            f"TempData/frames/tiff_{k}/Cells/fluo1/{n}.png", fluo1
                         )
                         n += 1
         return num_tiff
@@ -472,17 +427,11 @@ class ExtractionCrudBase:
         )
         async with async_session() as session:
             cell_id = f"F{i}C{j}"
-            img_ph = await self.load_image(
-                os.path.join(
-                    "TempData", "frames", f"tiff_{i}", "Cells", "ph", f"{j}.png"
-                )
-            )
+            img_ph = await self.load_image(f"TempData/frames/tiff_{i}/Cells/ph/{j}.png")
             img_fluo1 = img_fluo2 = None
             if self.mode != "single_layer":
                 img_fluo1 = await self.load_image(
-                    os.path.join(
-                        "TempData", "frames", f"tiff_{i}", "Cells", "fluo1", f"{j}.png"
-                    )
+                    f"TempData/frames/tiff_{i}/Cells/fluo1/{j}.png"
                 )
 
             contour, img_ph_gray, img_fluo1_gray, img_fluo2_gray = self.process_image(
@@ -499,14 +448,7 @@ class ExtractionCrudBase:
                     img_fluo1_data = cv2.imencode(".png", img_fluo1_gray)[1].tobytes()
                 if self.mode == "triple_layer":
                     img_fluo2 = await self.load_image(
-                        os.path.join(
-                            "TempData",
-                            "frames",
-                            f"tiff_{i}",
-                            "Cells",
-                            "fluo2",
-                            f"{j}.png",
-                        )
+                        f"TempData/frames/tiff_{i}/Cells/fluo2/{j}.png"
                     )
                     img_fluo2_gray = cv2.cvtColor(img_fluo2, cv2.COLOR_BGR2GRAY)
                     img_fluo2_data = cv2.imencode(".png", img_fluo2_gray)[1].tobytes()
@@ -535,11 +477,9 @@ class ExtractionCrudBase:
     async def main(self):
         chores = SyncChores()
         dbname = (
-            os.path.join("databases", f"{self.file_prefix}-uploaded.db")
+            f"databases/{self.file_prefix}-uploaded.db"
             if self.mode != "single_layer"
-            else os.path.join(
-                "databases", f"{self.file_prefix}-single_layer-uploaded.db"
-            )
+            else f"databases/{self.file_prefix}-single_layer-uploaded.db"
         )
 
         try:
@@ -572,7 +512,7 @@ class ExtractionCrudBase:
 
         tasks = []
         for i in range(iter_n[self.mode]):
-            cell_path = os.path.join("TempData", "frames", f"tiff_{i}", "Cells", "ph")
+            cell_path = f"TempData/frames/tiff_{i}/Cells/ph/"
             cell_files = await asyncio.to_thread(os.listdir, cell_path)
             for j in range(len(cell_files)):
                 tasks.append(self.process_cell(dbname, i, j))
@@ -586,11 +526,11 @@ class ExtractionCrudBase:
 
     async def delete_nd2_file(self, filename: str) -> bool:
         filename = filename.split("/")[-1]
-        await asyncio.to_thread(os.remove, os.path.join("uploaded_files", filename))
+        await asyncio.to_thread(os.remove, f"uploaded_files/{filename}")
         return True
 
     async def get_ph_contours(self, frame_num: int) -> StreamingResponse:
-        filepath = os.path.join("ph_contours", f"{frame_num}.png")
+        filepath = f"ph_contours/{frame_num}.png"
         if not os.path.exists(filepath):
             raise HTTPException(status_code=404, detail="File not found")
         return StreamingResponse(open(filepath, "rb"), media_type="image/png")
