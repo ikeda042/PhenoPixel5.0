d36306814e28e7a74389b28fa56425c4656cc677
ikeda042 <ikeda042main@gmail.com>
2024-07-21 23:24:35 +0900

    label

--- backend/app/CellDBConsole/crud.py
+++ backend/app/CellDBConsole/crud.py
@@ -1310,7 +1310,9 @@ class CellCrudBase:
         buf.seek(0)
         return StreamingResponse(buf, media_type="text/csv")
 
-    async def get_peak_paths_csv(self, degree: int = 3) -> StreamingResponse:
+    async def get_peak_paths_csv(
+        self, degree: int = 3, label: str = 1
+    ) -> StreamingResponse:
         cell_ids = await self.read_cell_ids()
         cells = [await self.read_cell(cell.cell_id) for cell in cell_ids]
         paths = await asyncio.gather(
