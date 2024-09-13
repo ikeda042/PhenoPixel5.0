6f8d378911184740e33f67405456f659bdac08f4
ikeda042 <ikeda042main@gmail.com>
2024-09-14 02:20:11 +0900

    sed

--- docker/create_env.sh
+++ docker/create_env.sh
@@ -14,7 +14,7 @@ fi
 cp "$TEMPLATE_FILE" "$OUTPUT_FILE"
 
 # Replace HOST_NAME= with HOST_NAME=localhost in the .env file
-sed -i 's/HOST_NAME=/HOST_NAME=localhost/' "$OUTPUT_FILE"
+sed -i '' 's/HOST_NAME=/HOST_NAME=localhost/' "$OUTPUT_FILE"
 
 # Output success message
 echo ".env file has been successfully generated with HOST_NAME=localhost."
