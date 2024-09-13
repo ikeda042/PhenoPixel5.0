ca1b036e522c429b2a0e69f3fdf6255c198e1dad
ikeda042 <ikeda042main@gmail.com>
2024-09-14 02:31:50 +0900

    host name

--- frontend/src/settings.ts
+++ frontend/src/settings.ts
@@ -1,11 +1,7 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
 const host_name = process.env.HOST_NAME || "localhost";
-const url_prefix = host_name + ":8000/api"
+const url_prefix = `${host_name}:8000/api`;
 
 export const settings = {
     url_prefix: url_prefix,
     api_url: url_prefix + "",
-}
+};
