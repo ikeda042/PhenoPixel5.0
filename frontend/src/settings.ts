cf2a84af9f6e7998dbff2fce6d0d0c6be830be40
ikeda042 <ikeda042main@gmail.com>
2024-09-14 03:10:43 +0900

    LT

--- frontend/src/settings.ts
+++ frontend/src/settings.ts
@@ -1,5 +1,5 @@
 const host_name = process.env.HOST_NAME || "localhost";
-const url_prefix = `${host_name}:8000/api`;
+const url_prefix = `http://${host_name}:8000/api`;
 
 export const settings = {
     url_prefix: url_prefix,
