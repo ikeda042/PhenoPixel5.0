a98821b546bbb60eb354fc74322fa05114c67586
ikeda042 <ikeda042main@gmail.com>
2024-10-05 01:09:37 +0900

    DH

--- backend/app/TimeLapseEngine/router.py
+++ backend/app/TimeLapseEngine/router.py
@@ -13,7 +13,9 @@ router_tl_engine = APIRouter(prefix="/tl-engine_x100", tags=["tl_engine_x100"])
 
 @router_tl_engine.post("/nd2_files")
 async def upload_nd2_file(file: UploadFile):
-    file_path = file.filename + "_timelapse"
+    filename = file.filename.split(".")[0]
+    ext = file.filename.split(".")[1]
+    file_path = filename + "_timelapse." + ext
     file_path = os.path.join("uploaded_files", file_path)
     try:
         async with aiofiles.open(file_path, "wb") as out_file:
@@ -21,7 +23,7 @@ async def upload_nd2_file(file: UploadFile):
                 await out_file.write(content)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-    return JSONResponse(content={"filename": file.filename + "_timelapse"})
+    return JSONResponse(content={"filename": file.filename})
 
 
 @router_tl_engine.get("/nd2_files")
