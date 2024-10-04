e18eb1412a94d43264a28b037ebf529452c6dcc1
ikeda042 <ikeda042main@gmail.com>
2024-10-05 00:44:10 +0900

    TLENGINE

---
+++ backend/app/TimeLapseEngine/router.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+from CellExtraction.crud import ExtractionCrudBase
+from fastapi.responses import JSONResponse, StreamingResponse, FileResponse
+from typing import Literal
+import os
+from fastapi import UploadFile
+import aiofiles
+from fastapi import HTTPException
+import shutil
+
+router_tl_engine = APIRouter(prefix="/tl-engin_x100", tags=["tl_engine_x100"])
