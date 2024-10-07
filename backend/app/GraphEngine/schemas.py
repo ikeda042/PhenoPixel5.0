b4d0170246208d452fab7fad617b9a5db738ac28
ikeda042 <ikeda042main@gmail.com>
2024-10-07 17:05:05 +0900

    class HeatMapVectorRel:

--- backend/app/GraphEngine/schemas.py
+++ backend/app/GraphEngine/schemas.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 
 
 @dataclass
-class HeatMapVectorBase:
+class HeatMapVectorAbs:
     index: int
     u1: list[float]
     G: list[float]
@@ -12,10 +12,19 @@ class HeatMapVectorBase:
     def __repr__(self) -> str:
         return f"u1: {self.u1}\nG: {self.G}"
 
-    def __gt__(self, other: HeatMapVectorBase) -> bool:
+    def __gt__(self, other: HeatMapVectorAbs) -> bool:
         return self.length > other.length
 
 
 @dataclass
-class HeatMapVectorAbs(HeatMapVectorBase):
-    pass
+class HeatMapVectorRel:
+    index: int
+    u1: list[float]
+    G: list[float]
+    length: float
+
+    def __repr__(self) -> str:
+        return f"u1: {self.u1}\nG: {self.G}"
+
+    def __gt__(self, other: HeatMapVectorAbs) -> bool:
+        return sum(self.G) > sum(other.G)
