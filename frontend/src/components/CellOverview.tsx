8321d9b2e4d9150aec35fda68033b160938f0479
ikeda042 <ikeda042main@gmail.com>
2024-10-08 18:04:33 +0900

    T1

--- frontend/src/components/CellOverview.tsx
+++ frontend/src/components/CellOverview.tsx
@@ -587,7 +587,7 @@ const CellImageGrid: React.FC = () => {
                                             <MenuItem value="light">Light</MenuItem>
                                             <MenuItem value="replot">Replot</MenuItem>
                                             <MenuItem value="path">Peak-path</MenuItem>
-                                            <MenuItem value="prediction">Prediction: T1</MenuItem>
+                                            <MenuItem value="prediction">Model T1</MenuItem>
                                         </Select>
                                     </FormControl>
                                 </Grid>
