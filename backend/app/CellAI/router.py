13fb14feb9156f440b4c27bc8ae4b3a89af141d0
ikeda042 <ikeda042main@gmail.com>
2024-10-08 17:51:58 +0900

    T1

--- backend/app/CellAI/router.py
+++ backend/app/CellAI/router.py
@@ -10,6 +10,4 @@ router_cell_ai = APIRouter(prefix="/cell_ai", tags=["cell_ai"])
 async def get_predicted_contour(
     db_name: str, cell_id: str, model: Literal["T1", "T2"] = "T1"
 ):
-    return await CellAiCrudBase(
-        db_name,
-    ).predict_contour(cell_id)
+    return await CellAiCrudBase(db_name, model_path=model).predict_contour(cell_id)
