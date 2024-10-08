28a633f3802ca3a7dfbf0d4956986a2f0e685229
ikeda042 <ikeda042main@gmail.com>
2024-10-08 17:51:04 +0900

    RT

--- backend/app/CellAI/router.py
+++ backend/app/CellAI/router.py
@@ -1,10 +1,7 @@
 from fastapi import APIRouter
 from CellAI.crud import CellAiCrudBase
-from fastapi.responses import JSONResponse, StreamingResponse, FileResponse
 from typing import Literal
-import os
-from fastapi import UploadFile
-from fastapi import HTTPException
+
 
 router_cell_ai = APIRouter(prefix="/cell_ai", tags=["cell_ai"])
 
