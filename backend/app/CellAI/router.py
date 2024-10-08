421d1bd28fbfeae9bfb14801b0ff97312ce3b990
ikeda042 <ikeda042main@gmail.com>
2024-10-08 17:44:50 +0900

    PTH

--- backend/app/CellAI/router.py
+++ backend/app/CellAI/router.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter
-from CellDBConsole.crud import CellCrudBase, AsyncChores
-from CellDBConsole.schemas import CellMorhology
+from CellAI.crud import CellAiCrudBase
 from fastapi.responses import JSONResponse, StreamingResponse, FileResponse
 from typing import Literal
 import os
@@ -8,3 +7,10 @@ from fastapi import UploadFile
 from fastapi import HTTPException
 
 router_cell_ai = APIRouter(prefix="/cell_ai", tags=["cell_ai"])
+
+
+@router_cell_ai.get("/{db_name}/{cell_id}")
+async def get_cell_ph(db_name: str, cell_id: str):
+    return await CellAiCrudBase(db_name, "CellAI/models/T1.pth").predict_contour(
+        cell_id
+    )
