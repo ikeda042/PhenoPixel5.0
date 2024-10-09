6e75fd1edf9ba496cb0f49d880e22615f12e9d01
ikeda042 <ikeda042main@gmail.com>
2024-10-09 16:21:18 +0900

    DP crud

---
+++ backend/app/Dropbox/crud.py
@@ -0,0 +1,29 @@
+import dropbox
+import aiohttp
+import aiofiles
+import asyncio
+import dropbox
+from dropbox.files import WriteMode
+from dotenv import load_dotenv
+from dropbox import Dropbox
+
+
+class DropboxCrud:
+    def __init__(
+        self,
+        token: str = load_dotenv().parsed.get("DROPBOX_ACCESS_TOKEN"),
+    ):
+        self.dbx: Dropbox = dropbox.Dropbox(token)
+
+    async def upload_file(self, file_path: str, file_name: str) -> None:
+        async with aiohttp.ClientSession() as session:
+            async with aiofiles.open(file_path, mode="r") as f:
+                await session.post(
+                    "https://content.dropboxapi.com/2/files/upload",
+                    headers={
+                        "Authorization": f"Bearer {self.dbx._oauth2_access_token}",
+                        "Dropbox-API-Arg": f'{{"path": "/PhenoPixelDatabases/{file_name}", "mode": "add", "autorename": true, "mute": false, "strict_conflict": false}}',
+                        "Content-Type": "application/octet-stream",
+                    },
+                    data=await f.read(),
+                )
