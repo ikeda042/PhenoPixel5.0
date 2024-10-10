5c25dea38cce1cbdc69a141a234748ca85d45977
ikeda042 <ikeda042main@gmail.com>
2024-10-10 16:26:18 +0900

    IP

--- backend/app/CellDBConsole/crud.py
+++ backend/app/CellDBConsole/crud.py
@@ -1515,10 +1515,6 @@ class CellCrudBase:
             file_path=f"databases/{dbname_cleaned.replace('-uploaded.db','')}-completed.db",
             file_name=f"databases/{dbname_cleaned.replace('-uploaded.db','')}-completed-{date}.db",
         )
-        print("#########################################")
-        print("#########################################")
-        print("#########################################")
-        print(status)
         return True
 
     async def check_if_database_updated(self):
