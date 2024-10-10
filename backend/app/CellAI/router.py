d7a94df7342bda5bd070d4877ec3611191ab5905
ikeda042 <ikeda042main@gmail.com>
2024-10-10 18:14:23 +0900

    TYPE

--- backend/app/CellAI/router.py
+++ backend/app/CellAI/router.py
@@ -11,3 +11,14 @@ async def get_predicted_contour(
     db_name: str, cell_id: str, model: Literal["T1", "T2", "T3"] = "T1"
 ):
     return await CellAiCrudBase(db_name, model_path=model).predict_contour(cell_id)
+
+
+@router_cell_ai.get("/{db_name}/{cell_id}/plot_data")
+async def get_plot_data(
+    db_name: str, cell_id: str, model: Literal["T1", "T2", "T3"] = "T1"
+):
+    data: list[list[float]] = await CellAiCrudBase(
+        db_name, model_path=model
+    ).predict_contour_draw(cell_id)
+    print(data)
+    return data
