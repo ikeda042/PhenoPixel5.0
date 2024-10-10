605359461b2cbe000d1f3f84415fb8ed1ea33e7f
ikeda042 <ikeda042main@gmail.com>
2024-10-10 13:33:46 +0900

    AWIT

--- backend/app/Dropbox/crud.py
+++ backend/app/Dropbox/crud.py
@@ -20,18 +20,18 @@ class DropboxCrud:
         self.dbx: Dropbox = dropbox.Dropbox(token)
 
     async def upload_file(self, file_path: str, file_name: str) -> str:
+        async with aiofiles.open(file_path, mode="rb") as f:
+            data = await f.read()
         async with aiohttp.ClientSession() as session:
-            async with aiofiles.open(file_path, mode="rb") as f:
-                data = await f.read()
-                await session.post(
-                    "https://content.dropboxapi.com/2/files/upload",
-                    headers={
-                        "Authorization": f"Bearer {self.dbx._oauth2_access_token}",
-                        "Dropbox-API-Arg": f'{{"path": "/PhenoPixelDatabases/{file_name}", "mode": "add", "autorename": true, "mute": false, "strict_conflict": false}}',
-                        "Content-Type": "application/octet-stream",
-                    },
-                    data=data,
-                )
+            await session.post(
+                "https://content.dropboxapi.com/2/files/upload",
+                headers={
+                    "Authorization": f"Bearer {self.dbx._oauth2_access_token}",
+                    "Dropbox-API-Arg": f'{{"path": "/PhenoPixelDatabases/{file_name}", "mode": "add", "autorename": true, "mute": false, "strict_conflict": false}}',
+                    "Content-Type": "application/octet-stream",
+                },
+                data=data,
+            )
         return f"File {file_name} uploaded successfully"
 
     async def list_files(self) -> list:
