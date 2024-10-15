b0908f55e3ef0a9e28ff838e47b22e8cea78264d
ikeda042 <ikeda042main@gmail.com>
2024-10-16 00:26:00 +0900

    UT

--- backend/app/testing/database/conftest.py
+++ backend/app/testing/database/conftest.py
@@ -1,16 +1,16 @@
 import os
-import sys
 
-# backend ディレクトリをモジュール検索パスに追加
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../..")))
-
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 import pytest
 from httpx import AsyncClient
-from backend.app.main import app  # FastAPI アプリケーションをインポート
+from app.main import app  # FastAPI などのアプリケーションをインポート
 
 
 @pytest.fixture(scope="session")
 def anyio_backend():
+    """
+    See https://anyio.readthedocs.io/en/stable/testing.html#using-async-fixtures-with-higher-scopes
+    """
     return "asyncio"
 
 
