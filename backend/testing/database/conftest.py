90b0ddb0526d00d77149c09eabaaa943a29cbc46
ikeda042 <ikeda042main@gmail.com>
2024-10-16 00:24:44 +0900

    init

--- backend/testing/database/conftest.py
+++ backend/testing/database/conftest.py
@@ -1,17 +1,20 @@
+import os
+import sys
+
+# backend ディレクトリをモジュール検索パスに追加
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../..")))
+
 import pytest
 from httpx import AsyncClient
-from backend.app.main import app
+from backend.app.main import app  # FastAPI アプリケーションをインポート
 
 
 @pytest.fixture(scope="session")
 def anyio_backend():
-    """
-    See https://anyio.readthedocs.io/en/stable/testing.html#using-async-fixtures-with-higher-scopes
-    """
     return "asyncio"
 
 
 @pytest.fixture(scope="session")
 async def client():
-    async with AsyncClient(app=app, base_url="") as client:
+    async with AsyncClient(app=app, base_url="http://testserver") as client:
         yield client
--- backend/testing/database/test_database.py
+++ backend/testing/database/test_database.py
@@ -1,6 +1,14 @@
 import pytest
+from httpx import AsyncClient
 
 
 @pytest.mark.anyio
 async def test_database():
     assert True
+
+
+@pytest.mark.anyio
+async def test_database_healthcheck(client: AsyncClient):
+    response = await client.get("/api/cells/database/healthcheck")
+    assert response.status_code == 200
+    assert response.json() == {"status": "ok"}
