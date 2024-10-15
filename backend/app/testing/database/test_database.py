1c7ee67a4934785cd79d446e2f2ee5322451b77b
ikeda042 <ikeda042main@gmail.com>
2024-10-16 00:50:04 +0900

    test

--- backend/app/testing/database/test_database.py
+++ backend/app/testing/database/test_database.py
@@ -3,7 +3,7 @@ from httpx import AsyncClient
 import os
 
 
-test_db_path: str = "../../databases/test_database.db"
+test_db_path: str = "../../databases"
 
 
 @pytest.mark.anyio
@@ -14,12 +14,4 @@ async def test_database():
 @pytest.mark.anyio
 async def test_database_healthcheck(client: AsyncClient):
     response = await client.get("/api/cells/database/healthcheck")
-    assert response.status_code == 200
-
-
-@pytest.mark.anyio
-async def test_database_read_cell_ids(client: AsyncClient):
-    # http://localhost:8000/api/cells/test_database.db/1
-    response = await client.get("/api/cells/.../databases/test_database.db/1")
-    print(os.listdir("../../databases"))
     assert response.status_code == 200
