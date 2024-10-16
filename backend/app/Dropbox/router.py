6d52c69a3390292721e2ff938468f8ef250815c5
ikeda042 <ikeda042main@gmail.com>
2024-10-16 23:43:54 +0900

    backup

--- backend/app/Dropbox/router.py
+++ backend/app/Dropbox/router.py
@@ -14,3 +14,9 @@ async def upload_file(file_path: str, file_name: str):
 @router_dropbox.get("/list")
 async def list_files():
     return {"files": await DropboxCrud().list_files()}
+
+
+@router_dropbox.post("/databases/backup")
+async def backup_databases(file_names: list[str]):
+    await DropboxCrud().backup_databases(file_names)
+    return {"message": "Backup completed"}
