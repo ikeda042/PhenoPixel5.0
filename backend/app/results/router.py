7fcc45f415ed817a45c44ea3d11a22b2d7ad3b10
ikeda042 <ikeda042main@gmail.com>
2024-10-16 21:01:14 +0900

    re

--- backend/app/results/router.py
+++ backend/app/results/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from results.crud import ResultsCRUD
-from typing import Literal
+from fastapi.responses import FileResponse
 
 
 router_results = APIRouter(prefix="/results", tags=["results"])
@@ -9,3 +9,8 @@ router_results = APIRouter(prefix="/results", tags=["results"])
 @router_results.get("")
 async def get_files():
     return await ResultsCRUD.read_result_files()
+
+
+@router_results.get("/{file_name}")
+async def get_file(file_name: str):
+    return FileResponse(f"results/{file_name}")
