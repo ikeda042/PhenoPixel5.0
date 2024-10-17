9e651ad1459d59f1fbf6b62c470039bd6e208a91
ikeda042 <ikeda042main@gmail.com>
2024-10-17 14:39:07 +0900

    DP

--- backend/app/Dropbox/crud.py
+++ backend/app/Dropbox/crud.py
@@ -8,18 +8,30 @@ from dropbox import Dropbox
 import os
 
 load_dotenv()
+APP_KEY = os.getenv("DROPBOX_APP_KEY")
+APP_SECRET = os.getenv("DROPBOX_APP_SECRET")
 
 
 class DropboxCrud:
+    app_key: str = APP_KEY
+    app_secret: str = APP_SECRET
 
-    def __init__(
-        self,
-        token: str = os.getenv(
-            "DROPBOX_ACCESS_TOKEN",
-            "token",
-        ),
-    ):
-        self.dbx: Dropbox = dropbox.Dropbox(token)
+    @classmethod
+    async def get_access_token(cls) -> str:
+        auth_url = "https://api.dropboxapi.com/oauth2/token"
+        data = {
+            "grant_type": "client_credentials",
+            "client_id": APP_KEY,
+            "client_secret": APP_SECRET,
+        }
+
+        async with aiohttp.ClientSession() as session:
+            async with session.post(auth_url, data=data) as response:
+                if response.status == 200:
+                    response_data = await response.json()
+                    return response_data["access_token"]
+                else:
+                    raise Exception("Failed to get access token")
 
     async def upload_file(self, file_path: str, file_name: str) -> str:
         async with aiofiles.open(file_path, mode="rb") as f:
@@ -28,7 +40,7 @@ class DropboxCrud:
             await session.post(
                 "https://content.dropboxapi.com/2/files/upload",
                 headers={
-                    "Authorization": f"Bearer {self.dbx._oauth2_access_token}",
+                    "Authorization": f"Bearer {await DropboxCrud.get_access_token()}",
                     "Dropbox-API-Arg": f'{{"path": "/PhenoPixelDatabases/{file_name}", "mode": "add", "autorename": true, "mute": false, "strict_conflict": false}}',
                     "Content-Type": "application/octet-stream",
                 },
@@ -37,9 +49,10 @@ class DropboxCrud:
         return f"File {file_name} uploaded successfully"
 
     async def list_files(self) -> list:
+        dbx = Dropbox(await DropboxCrud.get_access_token())
         try:
             response = await asyncio.to_thread(
-                self.dbx.files_list_folder, "/PhenoPixelDatabases"
+                dbx.files_list_folder, "/PhenoPixelDatabases"
             )
             return [file.name for file in response.entries]
         except Exception as e:
