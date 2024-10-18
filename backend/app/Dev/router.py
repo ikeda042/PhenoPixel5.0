61825feef88305df921887a1a88c824a08935a25
ikeda042 <ikeda042main@gmail.com>
2024-10-18 15:53:06 +0900

    LOGIN

--- backend/app/Dev/router.py
+++ backend/app/Dev/router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, UploadFile, HTTPException
 from fastapi.responses import JSONResponse
 import os
 import aiofiles
+from Dev.crud import HINETLogin
 
 router_dev = APIRouter(prefix="/dev", tags=["dev"])
 
@@ -17,3 +18,10 @@ async def upload_xlsx_file(file: UploadFile):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     return JSONResponse(content={"filename": file.filename})
+
+
+@router_dev.post("/login")
+async def login_to_hinet():
+    hinet_login = HINETLogin()
+    await hinet_login.login()
+    return JSONResponse(content={"message": "Login successful."})
