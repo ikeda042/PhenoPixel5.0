8686fdafd0a4a2571b6b98ac806039c30c6498fd
ikeda042 <ikeda042main@gmail.com>
2024-10-19 00:44:32 +0900

    pytest

--- backend/app/testing/dropbox/test_dropbox.py
+++ backend/app/testing/dropbox/test_dropbox.py
@@ -16,3 +16,9 @@ async def test_dropbox_healthcheck(client: AsyncClient):
     response = await client.get("/api/dropbox/connection_check")
     assert response.status_code == 200
     assert response.json()["status"] == True
+
+
+@pytest.mark.anyio
+async def test_dropbox_access_token(client: AsyncClient):
+    response = await client.get("/api/dropbox/access_token")
+    assert response.status_code == 200
