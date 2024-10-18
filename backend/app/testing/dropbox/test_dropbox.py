b1a4242eddc286f5fb6901d8873171e97c8257b6
ikeda042 <ikeda042main@gmail.com>
2024-10-19 00:43:43 +0900

    test

---
+++ backend/app/testing/dropbox/test_dropbox.py
@@ -0,0 +1,18 @@
+import pytest
+from httpx import AsyncClient
+import os
+import asyncio
+
+os.chdir(os.path.dirname(os.path.abspath("..")))
+
+
+@pytest.mark.anyio
+async def test_dropbox():
+    assert True
+
+
+@pytest.mark.anyio
+async def test_dropbox_healthcheck(client: AsyncClient):
+    response = await client.get("/api/dropbox/connection_check")
+    assert response.status_code == 200
+    assert response.json()["status"] == True
