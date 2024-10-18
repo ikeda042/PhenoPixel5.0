adb20e891c7c4624aff0b82ed2cb62b823f9eb72
ikeda042 <ikeda042main@gmail.com>
2024-10-18 21:22:04 +0900

    3d

--- backend/app/CellDBConsole/router.py
+++ backend/app/CellDBConsole/router.py
@@ -282,4 +282,5 @@ async def update_label_experiment(db_name: str, request: MetadataUpdateRequest):
 @router_cell.get("/{db_name}/{cell_id}/3d")
 async def get_3d_plot(db_name: str, cell_id: str):
     await AsyncChores().validate_database_name(db_name)
-    return await CellCrudBase(db_name=db_name).get_cloud_points(cell_id=cell_id)
+    image_buf = await CellCrudBase(db_name=db_name).get_cloud_points(cell_id=cell_id)
+    return StreamingResponse(image_buf, media_type="image/png")
