da20820c772c2be174575287b0ed7b73f697c0e0
ikeda042 <ikeda042main@gmail.com>
2024-10-20 21:24:38 +0900

    auth crud

---
+++ backend/app/Auth/crud.py
@@ -0,0 +1,38 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+import bcrypt
+import os
+
+security = HTTPBasic()
+
+password_hash_hard_coded = os.getenv("PASSWORD_HASH_SECRET")
+
+
+class AuthCrud:
+    @classmethod
+    def hash_password(cls, password: str) -> str:
+        return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+    @classmethod
+    def verify_password(plain_password: str, hashed_password: str) -> bool:
+        return bcrypt.checkpw(
+            plain_password.encode("utf-8"), hashed_password.encode("utf-8")
+        )
+
+    def get_account(credentials: HTTPBasicCredentials = Depends(security)) -> str:
+        correct_username = "admin"
+        correct_password_hash = password_hash_hard_coded
+        if correct_password_hash is None:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Password hash not found",
+            )
+        if credentials.username != correct_username or not AuthCrud.verify_password(
+            credentials.password, correct_password_hash
+        ):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Incorrect username or password",
+                headers={"WWW-Authenticate": "Basic"},
+            )
+        return credentials.username
