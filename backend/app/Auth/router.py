ce27bfa5cd52f62e488744d18fca3fdf98e5afb4
ikeda042 <ikeda042main@gmail.com>
2024-10-20 21:34:59 +0900

    -> sync

---
+++ backend/app/Auth/router.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter, UploadFile, HTTPException
+from fastapi.responses import JSONResponse
+import os
+import aiofiles
+from Auth.crud import AuthCrud
+
+router_auth = APIRouter(prefix="/auth", tags=["auth"])
+
+
+@router_auth.post("/generate_password_hash")
+def generate_password_hash(password: str):
+    password_hash = AuthCrud.hash_password(password)
+    return JSONResponse(content={"password_hash": password_hash})
+
+
+@router_auth.post("/login")
+def verify_password(plain_password: str, hashed_password: str):
+    is_verified = AuthCrud.verify_password(plain_password, hashed_password)
+    return JSONResponse(content={"is_verified": is_verified})
