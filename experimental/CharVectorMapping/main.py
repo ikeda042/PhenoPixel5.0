7c09350c5dc1ff675b3a23e1d2d022984f953aab
ikeda042 <ikeda042main@gmail.com>
2024-11-09 20:33:58 +0900

    main

---
+++ experimental/CharVectorMapping/main.py
@@ -0,0 +1,35 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+import cv2
+import pickle
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import Column, Integer, String, FLOAT, BLOB
+
+Base = declarative_base()
+
+
+class Cell(Base):
+    __tablename__ = "cells"
+    id = Column(Integer, primary_key=True)
+    cell_id = Column(String)
+    label_experiment = Column(String)
+    manual_label = Column(Integer)
+    perimeter = Column(FLOAT)
+    area = Column(FLOAT)
+    img_ph = Column(BLOB)
+    img_fluo1 = Column(BLOB, nullable=True)
+    img_fluo2 = Column(BLOB, nullable=True)
+    contour = Column(BLOB)
+    center_x = Column(FLOAT)
+    center_y = Column(FLOAT)
+
+
+dbpath = "sqlite:///experimental/U-net_Pytorch/test_contour_label_data.db"
+engine = create_engine(dbpath)
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
