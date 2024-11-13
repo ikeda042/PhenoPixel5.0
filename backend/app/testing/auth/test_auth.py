59dcd8c379c2a2e8afb53cb05f166512ad776711
ikeda042 <ikeda042main@gmail.com>
2024-11-13 18:40:51 +0900

    unit-test protected

--- backend/app/testing/auth/test_auth.py
+++ backend/app/testing/auth/test_auth.py
@@ -9,6 +9,13 @@ os.chdir(os.path.dirname(os.path.abspath("..")))
 
 @pytest.mark.anyio
 async def test_protected_route(client: AsyncClient):
-    with patch("Auth.crud.Auth.get_account", return_value="ikeda042"):
-        response = await client.get("/api/auth/login")
+    with patch("Auth.crud.Auth.verify_password", return_value=True):
+        response = await client.post(
+            "/api/auth/login",
+            params={
+                "plain_password": "test",
+                "hashed_password": "hashed_test",
+            },
+        )
         assert response.status_code == 200
+        assert response.json() == {"is_verified": True}
