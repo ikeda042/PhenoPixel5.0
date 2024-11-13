1c542ad77af135a77cbedc5cb89deb27848c5384
ikeda042 <ikeda042main@gmail.com>
2024-11-13 18:45:02 +0900

    unit -test mock -> get_account

--- backend/app/testing/dropbox/test_dropbox.py
+++ backend/app/testing/dropbox/test_dropbox.py
@@ -1,7 +1,8 @@
 import pytest
 from httpx import AsyncClient
 import os
-
+from unittest.mock import patch
+import base64
 
 os.chdir(os.path.dirname(os.path.abspath("..")))
 
@@ -20,5 +21,14 @@ async def test_dropbox_healthcheck(client: AsyncClient):
 
 @pytest.mark.anyio
 async def test_dropbox_access_token(client: AsyncClient):
-    response = await client.get("/api/dropbox/access_token")
-    assert response.status_code == 200
+    username = "admin"
+    password = "test"
+    credentials = f"{username}:{password}"
+    encoded_credentials = base64.b64encode(credentials.encode("utf-8")).decode("utf-8")
+    headers = {"Authorization": f"Basic {encoded_credentials}"}
+
+    with patch("Auth.crud.Auth.get_account", return_value="admin"), patch(
+        "Auth.crud.Auth.verify_password", return_value=True
+    ):
+        response = await client.get("/api/dropbox/access_token", headers=headers)
+        assert response.status_code == 200
