eba67b9c542cbd15ebc72538d978b9fec49b1361
ikeda042 <ikeda042main@gmail.com>
2024-11-13 23:12:53 +0900

    pytest -options

--- backend/app/run_tests.sh
+++ backend/app/run_tests.sh
@@ -8,6 +8,6 @@ find "$root_dir" -type f -name "test_*.py" | while read test_file; do
     test_dir=$(dirname "$test_file")
     echo "Running pytest in $test_dir"
     cd "$test_dir" || exit 1
-    pytest "$(basename "$test_file")"
+    pytest -v -s --tb=long "$(basename "$test_file")"
     cd - > /dev/null
 done
