6c79fd481eb75887c758eda472f0990bf95b069f
ikeda042 <ikeda042main@gmail.com>
2024-11-13 23:10:56 +0900

    unit test -sh

--- backend/app/run_tests.sh
+++ backend/app/run_tests.sh
@@ -5,6 +5,9 @@ root_dir="./testing"
 
 # Find all test_---.py files in subdirectories and run pytest for each
 find "$root_dir" -type f -name "test_*.py" | while read test_file; do
-    echo "Running pytest for $test_file"
-    pytest "$test_file"
+    test_dir=$(dirname "$test_file")
+    echo "Running pytest in $test_dir"
+    cd "$test_dir" || exit 1
+    pytest "$(basename "$test_file")"
+    cd - > /dev/null
 done
