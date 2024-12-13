2c27d157e97d4e8d7ce11abf8d2a60562324ad73
ikeda042 <ikeda042main@gmail.com>
2024-12-13 14:41:13 +0900

    AR

--- experimental/Autoencoder/inference.py
+++ experimental/Autoencoder/inference.py
@@ -3,6 +3,101 @@ import torch.nn as nn
 import cv2
 import numpy as np
 from typing import Union, Tuple
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+import cv2
+import pickle
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import Column, Integer, String, Float, BLOB
+import os
+import shutil
+
+# ディレクトリを空にする
+dir_path = "experimental/Autoencoder/images/infer_data/"
+if os.path.exists(dir_path):
+    shutil.rmtree(dir_path)
+
+os.makedirs(dir_path, exist_ok=True)
+Base = declarative_base()
+
+
+class Cell(Base):
+    __tablename__ = "cells"
+    id = Column(Integer, primary_key=True)
+    cell_id = Column(String)
+    label_experiment = Column(String)
+    manual_label = Column(Integer)
+    perimeter = Column(Float)
+    area = Column(Float)
+    img_ph = Column(BLOB)
+    img_fluo1 = Column(BLOB, nullable=True)
+    img_fluo2 = Column(BLOB, nullable=True)
+    contour = Column(BLOB)
+    center_x = Column(Float)
+    center_y = Column(Float)
+
+
+dbname = "sk394_bta_19.db"
+dbpath = "sqlite:///experimental/Autoencoder/" + dbname
+engine = create_engine(dbpath)
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+def parse_image(cell: Cell) -> tuple:
+    img_ph = cv2.imdecode(np.frombuffer(cell.img_ph, np.uint8), cv2.IMREAD_COLOR)
+    contour = pickle.loads(cell.contour)
+    img_fluo1 = cv2.imdecode(np.frombuffer(cell.img_fluo1, np.uint8), cv2.IMREAD_COLOR)
+    mask = np.zeros_like(img_ph)
+    cv2.drawContours(mask, [contour], -1, (255, 255, 255), -1)
+    masked = cv2.bitwise_and(img_fluo1, mask)
+    # maskedエリアを白(255)で塗りつぶす
+    return img_ph, masked
+
+
+def combine_images_grid(images, grid_size):
+    img_height, img_width, _ = images[0].shape
+    combined_image = np.zeros(
+        (img_height * grid_size, img_width * grid_size, 3), dtype=np.uint8
+    )
+
+    for i, img in enumerate(images):
+        row = i // grid_size
+        col = i % grid_size
+        combined_image[
+            row * img_height : (row + 1) * img_height,
+            col * img_width : (col + 1) * img_width,
+        ] = img
+
+    return combined_image
+
+
+cells_with_label_1 = session.query(Cell).filter(Cell.manual_label == 1).all()
+print(len(cells_with_label_1))
+for i, cell in enumerate(cells_with_label_1):
+    _, masked = parse_image(cell)
+    cv2.imwrite(
+        f"experimental/Autoencoder/images/infer_data/{cell.cell_id}.png", masked
+    )
+
+
+images = [
+    img
+    for img in (
+        cv2.imread(f"experimental/Autoencoder/images/infer_data/{cell.cell_id}.png")
+        for cell in cells_with_label_1
+    )
+    if img is not None
+]
+combined_image = combine_images_grid(images, 8)
+cv2.imwrite(
+    "experimental/Autoencoder/images/infer_dataset_combined.png", combined_image
+)
 
 
 class Autoencoder(nn.Module):
@@ -137,14 +232,14 @@ def process_image_with_autoencoder(
         return None, None
 
 
-if __name__ == "__main__":
-    model_path = "experimental/Autoencoder/AE.pth"
-    image_path = "sample_image.jpg"
+# if __name__ == "__main__":
+#     model_path = "experimental/Autoencoder/AE.pth"
+#     image_path = "sample_image.jpg"
 
-    mse_score, saved_path = process_image_with_autoencoder(
-        image_path=image_path, model_path=model_path
-    )
+#     mse_score, saved_path = process_image_with_autoencoder(
+#         image_path=image_path, model_path=model_path
+#     )
 
-    if mse_score is not None:
-        print(f"Reconstruction MSE Score: {mse_score:.6f}")
-        print(f"Reconstructed image saved to: {saved_path}")
+#     if mse_score is not None:
+#         print(f"Reconstruction MSE Score: {mse_score:.6f}")
+#         print(f"Reconstructed image saved to: {saved_path}")
