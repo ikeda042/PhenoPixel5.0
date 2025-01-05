e3b628e24b8bece378a0f81129b7f4d9d0aead49
ikeda042 <ikeda042main@gmail.com>
2025-01-05 23:40:22 +0900

    test all sh

--- backend/app/testing/test_all.sh
+++ backend/app/testing/test_all.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 echo "Running all tests"
-echo "Make sure server_dev.sh has already been executed for the payment related tests"
-
 for dir in */; do
     if [ -d "$dir" ] && [ "$(basename "$dir")" != ".pytest_cache" ] && [ "$(basename "$dir")" != "__pycache__" ]; then
         cd "$dir"
