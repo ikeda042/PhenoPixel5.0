573dd975bfb5d05449d25f674201c1a71d0a6509
Yunosuke Ikeda <ikeda042main@gmail.com>
2025-01-14 02:43:36 +0900

    Router

--- backend/app/TimeLapseEngine/router.py
+++ backend/app/TimeLapseEngine/router.py
@@ -83,19 +83,20 @@ async def delete_nd2_file(file_path: str):
     return JSONResponse(content={"detail": f"{file_path} deleted successfully."})
 
 
-# async def extract_cells(
-#         self,
-#         field: str,
-#         dbname: str = "cells_timelapse.db",
-#         param1: int = 130,
-#         min_area: int = 300,
-#     ):
-
-
 @router_tl_engine.get("/nd2_files/{file_name}/cells/{Field}")
 async def extract_cells(file_name: str, Field: str):
     """
     セルを抽出し、データベースに保存するエンドポイント。
     """
     await TimelapseEngineCrudBase(file_name).extract_cells(Field)
+    return JSONResponse(content={"message": "Cells extracted and saved to database."})
+
+
+@router_tl_engine.get("/nd2_files/{file_name}/cells")
+async def extract_all_cells(file_name: str):
+    """
+    全てのフィールドからセルを抽出し、データベースに保存するエンドポイント。
+    """
+    async for Field in TimelapseEngineCrudBase(file_name).get_fields_of_nd2():
+        await TimelapseEngineCrudBase(file_name).extract_cells(Field)
     return JSONResponse(content={"message": "Cells extracted and saved to database."})
