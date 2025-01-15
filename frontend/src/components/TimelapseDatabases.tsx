ca62f4a8155c8f74061ecf282feb7e88456f856d
Yunosuke Ikeda <ikeda042main@gmail.com>
2025-01-15 13:17:11 +0900

    TLB

--- frontend/src/components/TimelapseDatabases.tsx
+++ frontend/src/components/TimelapseDatabases.tsx
@@ -11,10 +11,14 @@ import {
   TableBody,
   Paper,
   Button,
+  IconButton,
+  Tooltip,
 } from "@mui/material";
 import axios from "axios";
 import { settings } from "../settings";
 import { useNavigate } from "react-router-dom";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
 
 /**
  * /tlengine/databases のレスポンス用インターフェイス
@@ -47,24 +51,47 @@ const TimelapseDatabases: React.FC = () => {
   }, []);
 
   /**
-   * 「Access Databases」 ボタンクリック時
-   * React Router を利用して /tlengine/databases に遷移
+   * クリップボードにコピー
    */
-  const handleAccessDatabases = () => {
-    navigate("/tlengine/databases");
+  const handleCopyToClipboard = (dbName: string) => {
+    navigator.clipboard
+      .writeText(dbName)
+      .then(() => {
+        alert(`${dbName} copied to clipboard!`);
+      })
+      .catch((err) => {
+        console.error("Failed to copy text: ", err);
+      });
+  };
+
+  /**
+   * 指定のDBに画面遷移 (クエリパラメータ付き)
+   * Databases.tsx での handleNavigate を参考に
+   */
+  const handleNavigate = (dbName: string) => {
+    // 例: /tlengine/databases?db_name=XXX
+    navigate(`/tlengine/databases?db_name=${dbName}`);
   };
 
   return (
     <Container>
       <Box mt={2}>
-        {/* タイトル + ボタン */}
-        <Box display="flex" alignItems="center" justifyContent="space-between" mb={2}>
+        {/* タイトル */}
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="space-between"
+          mb={2}
+        >
           <Typography variant="h5" gutterBottom>
             Timelapse Databases
           </Typography>
-          <Button variant="contained" onClick={handleAccessDatabases}>
+          {/* もし従来の「Access Databases」ボタンが必要なら残す */}
+          {/* 
+          <Button variant="contained" onClick={() => navigate("/tlengine/databases")}>
             Access Databases
           </Button>
+          */}
         </Box>
 
         {/* データベース一覧テーブル */}
@@ -73,12 +100,32 @@ const TimelapseDatabases: React.FC = () => {
             <TableHead>
               <TableRow>
                 <TableCell>Database Name</TableCell>
+                <TableCell>Copy</TableCell>
+                <TableCell>Access Database</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
               {databases.map((database) => (
                 <TableRow key={database}>
+                  {/* Database Name */}
                   <TableCell>{database}</TableCell>
+
+                  {/* Copy to Clipboard */}
+                  <TableCell>
+                    <Tooltip title="Copy to clipboard">
+                      <IconButton onClick={() => handleCopyToClipboard(database)}>
+                        <ContentCopyIcon />
+                      </IconButton>
+                    </Tooltip>
+                  </TableCell>
+
+                  {/* Access database */}
+                  <TableCell>
+                    <IconButton onClick={() => handleNavigate(database)}>
+                      <Typography>Access database</Typography>
+                      <NavigateNextIcon />
+                    </IconButton>
+                  </TableCell>
                 </TableRow>
               ))}
             </TableBody>
