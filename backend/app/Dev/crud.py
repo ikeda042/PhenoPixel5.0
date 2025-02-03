96dff21e1f02cb3cbf713566e17f780c9ba29731
Yunosuke Ikeda <ikeda042main@gmail.com>
2025-02-03 17:36:27 +0900

    class method <-

--- backend/app/Dev/crud.py
+++ backend/app/Dev/crud.py
@@ -10,87 +10,85 @@ load_dotenv()
 
 
 class HINETLogin:
-    def __init__(self) -> None:
-        self.driver = None
+    email = os.getenv("EMAIL", None)
+    password = os.getenv("PASSWORD", None)
+    hinet_url = os.getenv("HINET_URL", None)
+    driver = None
 
-        self.email = os.getenv("EMAIL", None)
-        self.password = os.getenv("PASSWORD", None)
-        self.hinet_url = os.getenv("HINET_URL", None)
+    @classmethod
+    async def start_driver(cls) -> None:
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        cls.driver = webdriver.Chrome(options=chrome_options)
 
-    async def start_driver(self) -> None:
-        self.chrome_options = Options()
-        self.chrome_options.add_argument("--headless")
-        self.chrome_options.add_argument("--no-sandbox")
-        self.chrome_options.add_argument("--disable-dev-shm-usage")
-
-        self.driver = webdriver.Chrome(options=self.chrome_options)
-
-    async def login(self) -> None:
-        await self.start_driver()
+    @classmethod
+    async def login(cls) -> None:
+        await cls.start_driver()
         print("driver started")
-        self.driver.get(self.hinet_url)
+        cls.driver.get(cls.hinet_url)
 
         await asyncio.sleep(1)
-        account_input = self.driver.find_element(By.ID, "i0116")
-        account_input.send_keys(self.email)
+        account_input = cls.driver.find_element(By.ID, "i0116")
+        account_input.send_keys(cls.email)
         print("email entered")
-        next_button = self.driver.find_element(By.ID, "idSIButton9")
+        next_button = cls.driver.find_element(By.ID, "idSIButton9")
         next_button.click()
         print("next button clicked")
         await asyncio.sleep(2)
 
-        password_input = self.driver.find_element(By.ID, "i0118")
-        password_input.send_keys(self.password)
+        password_input = cls.driver.find_element(By.ID, "i0118")
+        password_input.send_keys(cls.password)
         print("password entered")
-        signin_button = self.driver.find_element(By.ID, "idSIButton9")
+        signin_button = cls.driver.find_element(By.ID, "idSIButton9")
         signin_button.click()
         print("signin button clicked")
         await asyncio.sleep(2)
 
-        stay_signed_in_button = self.driver.find_element(By.ID, "idSIButton9")
+        stay_signed_in_button = cls.driver.find_element(By.ID, "idSIButton9")
         stay_signed_in_button.click()
         print("stay signed in button clicked")
         await asyncio.sleep(1)
 
         try:
-            registration_button = self.driver.find_element(
+            registration_button = cls.driver.find_element(
                 By.XPATH, '//button[contains(text(), "Registration")]'
             )
-
             registration_button.click()
-
             print("Registration button found and clicked.")
         except NoSuchElementException:
             pass
 
         await asyncio.sleep(2)
-        self.driver.quit()
+        cls.driver.quit()
 
-    async def close_driver(self):
-        if self.driver:
-            self.driver.quit()
+    @classmethod
+    async def close_driver(cls):
+        if cls.driver:
+            cls.driver.quit()
 
-    async def main():
-        hinet_login = HINETLogin()
-        if hinet_login.email and hinet_login.password and hinet_login.hinet_url:
-            await hinet_login.login()
+    @classmethod
+    async def main(cls):
+        if cls.email and cls.password and cls.hinet_url:
+            await cls.login()
         else:
             raise Exception(
                 "Please provide email, password and hinet url in .env file."
             )
 
+    @classmethod
+    async def git_pull(cls):
+        process_checkout = await asyncio.create_subprocess_shell(
+            "git checkout main",
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
+        await process_checkout.communicate()
 
-async def git_pull():
-    process_checkout = await asyncio.create_subprocess_shell(
-        "git checkout main",
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
-    )
-    await process_checkout.communicate()
-
-    process_pull = await asyncio.create_subprocess_shell(
-        "git pull",
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
-    )
-    await process_pull.communicate()
+        process_pull = await asyncio.create_subprocess_shell(
+            "git pull",
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
+        await process_pull.communicate()
