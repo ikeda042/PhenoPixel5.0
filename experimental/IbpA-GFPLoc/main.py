e47787d7cbc7da4fc1134793281185af12b5f222
ikeda042 <ikeda042main@gmail.com>
2025-02-21 22:05:12 +0900

    T

--- experimental/IbpA-GFPLoc/main.py
+++ experimental/IbpA-GFPLoc/main.py
@@ -58,7 +58,6 @@ class IbpaGfpLoc:
             )
         return image
 
-    @classmethod
     async def _get_cells(self) -> list[Cell]:
         async with self._async_session() as session:
             result = await session.execute(select(Cell))
@@ -80,68 +79,18 @@ class IbpaGfpLoc:
 
         ret, buffer = cv2.imencode(".png", img)
         if ret:
-            with open("output_image.png", "wb") as f:
+            with open("experimental/IbpA-GFPLoc/images/output_image.png", "wb") as f:
                 f.write(buffer)
 
         return {"status": "success", "message": "Image saved to output_image.png"}
 
-
-async def main():
-    engine = create_async_engine(
-        "sqlite+aiosqlite:///experimental/IbpA-GFPLoc/sk326gen120min.db?timeout=30",
-        echo=False,
-    )
-    async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
-    async with async_session() as session:
-        result = await session.execute(select(Cell))
-        cells = result.scalars().all()
+    async def main(self):
+        cells = await self._get_cells()
         print(cells)
-
-
-async def async_imdecode(data: bytes) -> np.ndarray:
-    loop = asyncio.get_running_loop()
-    with ThreadPoolExecutor(max_workers=10) as executor:
-        img = await loop.run_in_executor(
-            executor, cv2.imdecode, np.frombuffer(data, np.uint8), cv2.IMREAD_COLOR
-        )
-    return img
-
-
-async def draw_contour(
-    image: np.ndarray, contour: bytes, thickness: int = 1
-) -> np.ndarray:
-    """
-    輪郭を画像上に描画。
-    """
-    loop = asyncio.get_running_loop()
-    with ThreadPoolExecutor(max_workers=10) as executor:
-        contour = pickle.loads(contour)
-        image = await loop.run_in_executor(
-            executor,
-            lambda: cv2.drawContours(image, contour, -1, (0, 255, 0), thickness),
-        )
-    return image
-
-
-async def parse_image(
-    data: bytes,
-    contour: bytes | None = None,
-    scale_bar: bool = False,
-    brightness_factor: float = 1.0,
-):
-    img = await async_imdecode(data)
-    if contour:
-        img = await draw_contour(img, contour)
-    if brightness_factor != 1.0:
-        img = cv2.convertScaleAbs(img, alpha=brightness_factor, beta=0)
-
-    ret, buffer = cv2.imencode(".png", img)
-    if ret:
-        with open("output_image.png", "wb") as f:
-            f.write(buffer)
-
-    return {"status": "success", "message": "Image saved to output_image.png"}
+        cell1 = cells[0]
+        await self._parse_image(cell1.img_ph, cell1.contour)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    ibpa_gfp_loc = IbpaGfpLoc()
+    asyncio.run(ibpa_gfp_loc.main())
