7445116ec973da0bee59826c633d8cb4024615d1
Yunosuke Ikeda <ikeda042main@gmail.com>
2025-02-28 19:20:56 +0900

    Router

--- backend/app/OAuth2/router.py
+++ backend/app/OAuth2/router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from .schemas import Tokens
 from .login_manager import authorize
-
+from .crud import UserCrud
 
 router_oauth2 = APIRouter(prefix="/oauth2", tags=["oauth2"])
 
@@ -11,3 +11,8 @@ router_oauth2 = APIRouter(prefix="/oauth2", tags=["oauth2"])
 )
 async def get_token(token=Depends(authorize)):
     return token
+
+
+@router_oauth2.post("/register", description="ユーザー登録用エンドポイント")
+async def register_user(user=Depends(UserCrud.create)):
+    return user
