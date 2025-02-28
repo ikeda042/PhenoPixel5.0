df63252906022b6119723b8a0e3e7fe94ea550ac
Yunosuke Ikeda <ikeda042main@gmail.com>
2025-02-28 19:15:08 +0900

    router

--- backend/app/OAuth2/router.py
+++ backend/app/OAuth2/router.py
@@ -1,4 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from .schemas import Tokens
+from .login_manager import authorize
 
 
 router_oauth2 = APIRouter(prefix="/oauth2", tags=["oauth2"])
+
+
+@router_oauth2.post(
+    "/token", response_model=Tokens, description="トークン取得用エンドポイント"
+)
+async def get_token(token=Depends(authorize)):
+    return token
