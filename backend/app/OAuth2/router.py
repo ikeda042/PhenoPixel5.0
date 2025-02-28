b192cb03860f9fd65d271090e16ba55ca9c0bc2d
Yunosuke Ikeda <ikeda042main@gmail.com>
2025-03-01 01:52:20 +0900

    /change pass

--- backend/app/OAuth2/router.py
+++ backend/app/OAuth2/router.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends
+from pydantic import BaseModel
 from .schemas import Tokens, UserCreate
 from .login_manager import authorize, get_account, get_account_optional
 from .crud import UserCrud
@@ -38,3 +39,18 @@ async def protected_optional(account=Depends(get_account_optional)):
 @router_oauth2.get("/me", description="自分の情報を取得するエンドポイント")
 async def me(account=Depends(get_account)):
     return {"account": account.dict()}
+
+
+class PasswordUpdate(BaseModel):
+    old_password: str
+    new_password: str
+
+
+@router_oauth2.put("/change_password", description="パスワード更新用エンドポイント")
+async def change_password(
+    data: PasswordUpdate, account=Depends(get_account), session=Depends(get_session)
+):
+    user = await UserCrud.change_password(
+        session, account.id, data.old_password, data.new_password
+    )
+    return {"account": user.dict()}
