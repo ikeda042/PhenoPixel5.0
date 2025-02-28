b7a9131cbb3f30da3c90167d1f025dd524bcdfa6
Yunosuke Ikeda <ikeda042main@gmail.com>
2025-02-28 17:03:26 +0900

    Settings

---
+++ backend/app/settings.py
@@ -0,0 +1,23 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    API_TITLE: str = "PhenoPixel5.0API"
+    API_PREFIX: str = "/api"
+    TEST_ENV: str = ""
+
+    ADMIN_PASSWORD_HASH: str | None = None
+
+    EMAIL: str | None = None
+    PASSWORD: str | None = None
+    HINET_URL: str | None = None
+
+    DROPBOX_REFRESH_TOKEN: str | None = None
+    DROPBOX_APP_KEY: str | None = None
+    DROPBOX_APP_SECRET: str | None = None
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
