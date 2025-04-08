02fe7c657a01dfe3e3ae5fdee03de16fb78e90b0
ikeda042 <ikeda042main@gmail.com>
2025-04-08 18:42:56 +0900

    T

--- experimental/DotPatternMap/main.py
+++ experimental/DotPatternMap/main.py
@@ -1028,7 +1028,7 @@ def main(db: str):
     # 512×128用フォルダの掃除
     clean_directory("experimental/DotPatternMap/images/map_64_normalized")
 
-    cells: list[Cell] = database_parser(db)[:10]
+    cells: list[Cell] = database_parser(db)[:]
     map64: Map64 = Map64()
     vectors = []
     for cell in tqdm(cells):
