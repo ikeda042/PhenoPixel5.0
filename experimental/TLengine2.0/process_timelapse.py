91ac50d479dac69cca98b150f7d02be530f9347d
ikeda042 <ikeda042main@gmail.com>
2025-04-11 13:46:20 +0900

    T

--- experimental/TLengine2.0/process_timelapse.py
+++ experimental/TLengine2.0/process_timelapse.py
@@ -148,8 +148,8 @@ if __name__ == "__main__":
     # 任意のField, Time, シフト量を指定
     test_field = 0       # 1つ目のField → Pythonで言えばindex = 0
     test_time = 0        # 1つ目のTime  → Pythonで言えばindex = 0
-    shift_vertical = 10  # 下に10ピクセル
-    shift_horizontal = -5  # 左に5ピクセル
+    shift_vertical = 100  # 下に10ピクセル
+    shift_horizontal = -50  # 左に5ピクセル
 
     # シフトしたph画像を取得
     shifted_ph_image = shift_ph_image(
