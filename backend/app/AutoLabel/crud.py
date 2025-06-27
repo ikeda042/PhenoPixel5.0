e8ad2b8f32c30244c8509cd52ef948661a28d463
Yunosuke Ikeda <ikeda042main@gmail.com>
2025-06-27 15:38:24 +0900

    fix autolabel model path

--- backend/app/AutoLabel/crud.py
+++ backend/app/AutoLabel/crud.py
@@ -8,7 +8,7 @@ import numpy as np
 from CellDBConsole.crud import CellCrudBase
 
 ROOT_DIR = Path(__file__).resolve().parents[2]
-MODEL_PATH = "AutoLabel/svm_cell_classifier.pkl"
+MODEL_PATH = Path(__file__).resolve().parent / "svm_cell_classifier.pkl"
 
 
 class AutoLabelCrud:
